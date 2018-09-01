cb011609612412d9455d491f4ed9cb2a610f0a91
Bhavir Shah <shahbhavir@gmail.com>
2018-09-01 13:27:51 +0530

    build: change core build target browsers

--- packages/walletconnect-core/rollup.config.js
+++ packages/walletconnect-core/rollup.config.js
@@ -37,11 +37,6 @@ export default [
   },
 
   // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // an array for the `output` option, where we can specify
-  // `file` and `format` for each target)
   {
     input: 'src/index.js',
     external: ['crypto'],
