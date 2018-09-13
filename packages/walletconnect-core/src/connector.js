1ebbe1c1f4d314b3622aa53524ba23f4baacf020
Pedro Gomes <pedrogomes94@gmail.com>
2018-09-12 17:28:07 -0700

    remove encryptMessage

--- packages/walletconnect-core/src/connector.js
+++ packages/walletconnect-core/src/connector.js
@@ -1,7 +1,6 @@
 /* global fetch Buffer */
 
 import crypto from 'crypto'
-import Ajv from 'ajv'
 
 import generateKey from './generateKey'
 import parseStandardURI from './parseStandardURI'
@@ -81,45 +80,6 @@ export default class Connector {
     }
 
     this._sessionId = value
-  }
-
-  get typedDataSchema() {
-    // From https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md#specification-of-the-eth_signtypeddata-json-rpc
-    return {
-      type: 'object',
-      properties: {
-        types: {
-          type: 'object',
-          additionalProperties: {
-            type: 'array',
-            items: {
-              type: 'object',
-              properties: {
-                name: { type: 'string' },
-                type: { type: 'string', enum: this._solidityTypes }
-              },
-              required: ['name', 'type']
-            }
-          }
-        },
-        primaryType: { type: 'string' },
-        domain: { type: 'object' },
-        message: { type: 'object' }
-      }
-    }
-  }
-
-  async encryptMessage(typedData, customIv = null) {
-    const ajv = new Ajv()
-    const valid = ajv.validate(this.typedDataSchema, typedData)
-
-    if (!valid) {
-      throw new Error(
-        'Data must follow the EIP712 standard. ' + ajv.errorsText()
-      )
-    }
-
-    return this.encrypt(typedData, customIv)
   }
 
   async encrypt(data, customIv = null) {
@@ -367,27 +327,5 @@ export default class Connector {
     }
 
     return decryptedData
-  }
-
-  //
-  // Get solidityTypes
-  //
-
-  get _solidityTypes() {
-    const types = ['bool', 'address', 'int', 'uint', 'string', 'byte']
-    const ints = Array.from(new Array(32)).map(
-      (e, index) => `int${(index + 1) * 8}`
-    )
-    const uints = Array.from(new Array(32)).map(
-      (e, index) => `uint${(index + 1) * 8}`
-    )
-    const bytes = Array.from(new Array(32)).map(
-      (e, index) => `bytes${index + 1}`
-    )
-
-    return types
-      .concat(ints)
-      .concat(uints)
-      .concat(bytes)
   }
 }
