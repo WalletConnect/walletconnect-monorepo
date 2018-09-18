1ae22e61ab4c696b64ef1b0e8ea507a06de5932d
Pedro Gomes <pedrogomes94@gmail.com>
2018-09-18 16:23:02 -0700

    merge conflicts

--- packages/walletconnect-web3-provider/test/index.spec.js
+++ packages/walletconnect-web3-provider/test/index.spec.js
@@ -1,5 +1,5 @@
 /* global describe it */
 
-describe('walletconnect-web3-provider', () => {
+describe('// ----------- walletconnect-web3-provider ----------- //', () => {
   it('needs tests', () => {})
 })
