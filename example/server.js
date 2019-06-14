619dca40dae80105d6431db429439f4a3cfb03f0
Pedro Gomes <pedrogomes94@gmail.com>
2019-06-14 14:24:50 +0200

    add plain html example

---
+++ example/server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+
+const PORT = 8060
+const ROOT_PATH = path.join(__dirname, '../')
+const STATIC_PATH = path.join(ROOT_PATH, 'example/public')
+
+const app = express()
+
+app.use(express.static(ROOT_PATH))
+app.use(express.static(STATIC_PATH, { index: ['index.html'] }))
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
