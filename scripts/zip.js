ef987a5e7f25a395590877082c4a661a4aa9cbcf
Pedro Gomes <pedrogomes94@gmail.com>
2019-06-18 15:42:11 +0200

    add npm zip script

--- scripts/zip.js
+++ scripts/zip.js
@@ -2,22 +2,21 @@ const fs = require('fs')
 const path = require('path')
 const archiver = require('archiver')
 
+const { getName, statPath, verifyDir, verifyFile } = require('./common')
+
 const ROOT_DIR = path.join(__dirname, '../')
-
 const PACKAGES_DIR = path.join(ROOT_DIR, './packages')
-console.log('PACKAGES_DIR', PACKAGES_DIR)
-
 const DIST_DIR = path.join(ROOT_DIR, './dist')
 
-async function archiveDir (inputPath, outputPath) {
-  return new Promise((resolve, reject) => {
+function archiveDir (inputPath, outputPath) {
+  return new Promise(async (resolve, reject) => {
+    await verifyFile(outputPath)
     const output = fs.createWriteStream(outputPath)
     const archive = archiver('zip', { zlib: { level: 9 } })
 
     output.on('close', () => {
-      console.log(archive.pointer() + ' total bytes')
       console.log(
-        'archiver has been finalized and the output file descriptor has closed.'
+        `Archived ${getName(outputPath)} (${archive.pointer()} bytes)`
       )
       resolve(true)
     })
@@ -27,7 +26,6 @@ async function archiveDir (inputPath, outputPath) {
       resolve(true)
     })
 
-    // good practice to catch warnings (ie stat failures and other non-blocking errors)
     archive.on('warning', function (err) {
       if (err.code === 'ENOENT') {
         console.log('WARN:', err.message)
@@ -35,7 +33,6 @@ async function archiveDir (inputPath, outputPath) {
         reject(err)
       }
     })
-    // pipe archive data to the file
     archive.pipe(output)
 
     archive.directory(inputPath, false)
@@ -44,33 +41,27 @@ async function archiveDir (inputPath, outputPath) {
   })
 }
 
-fs.readdir(PACKAGES_DIR, (err, files) => {
+fs.readdir(PACKAGES_DIR, async (err, files) => {
   if (err) {
     console.error('Could not list the directory.\n', err.message)
     process.exit(1)
   }
 
-  files.forEach((file, index) => {
-    console.log('file', file)
+  await verifyDir(DIST_DIR)
+
+  files.forEach(async (file, index) => {
     const filePath = path.join(PACKAGES_DIR, file)
 
-    fs.stat(filePath, async (error, stat) => {
-      if (error) {
-        console.error('Error stating file.', error)
-        return
-      }
+    const stat = await statPath(filePath)
 
-      if (stat.isDirectory()) {
-        console.log('filePath', filePath)
-        const inputPath = path.join(filePath, '/lib')
-        console.log('inputPath', inputPath)
-        const outputPath = path.join(DIST_DIR, file + '.zip')
-        try {
-          await archiveDir(inputPath, outputPath)
-        } catch (err) {
-          console.error(err)
-        }
+    if (stat.isDirectory()) {
+      const inputPath = path.join(filePath, '/lib')
+      const outputPath = path.join(DIST_DIR, file + '.zip')
+      try {
+        await archiveDir(inputPath, outputPath)
+      } catch (err) {
+        console.error(err)
       }
-    })
+    }
   })
 })
