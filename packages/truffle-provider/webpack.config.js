696866fe7cffd44840ab6154e0bea18882076535
Pedro Gomes <pedrogomes94@gmail.com>
2020-01-14 19:49:22 +0100

    truffle-provider

--- packages/truffle-provider/webpack.config.js
+++ packages/truffle-provider/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   mode: 'production',
   entry: {
-    index: './src/index.ts'
+    index: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'lib'),
@@ -22,17 +22,5 @@ module.exports = {
   },
   module: {
     rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }]
-  },
-  externals: {
-    ora: 'ora',
-    web3: 'web3',
-    url: 'url',
-    crypto: 'crypto',
-    ws: 'ws',
-    'web3-provider-engine': 'web3-provider-engine',
-    'web3-provider-engine/subproviders/filters': 'web3-provider-engine/subproviders/filters',
-    'web3-provider-engine/subproviders/nonce-tracker': 'web3-provider-engine/subproviders/nonce-tracker',
-    'web3-provider-engine/subproviders/hooked-wallet': 'web3-provider-engine/subproviders/hooked-wallet',
-    'web3-provider-engine/subproviders/provider': 'web3-provider-engine/subproviders/provider'
   }
 }
