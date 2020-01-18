f4e3c608d1110711a29ab9c74f949eb9e4011eb0
Pedro Gomes <pedrogomes94@gmail.com>
2020-01-18 15:03:15 +0100

    detect enviroment utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,5 +1,10 @@
 import BigNumber from 'bignumber.js'
-import { isHexString as _isHexString, hexlify, arrayify } from '@ethersproject/bytes'
+import { detect, BrowserInfo, BotInfo, NodeInfo } from 'detect-browser'
+import {
+  isHexString as _isHexString,
+  hexlify,
+  arrayify
+} from '@ethersproject/bytes'
 import { getAddress } from '@ethersproject/address'
 import { toUtf8Bytes, toUtf8String } from '@ethersproject/strings'
 
@@ -31,7 +36,10 @@ export function convertArrayBufferToUtf8 (arrayBuffer: ArrayBuffer): string {
   return utf8
 }
 
-export function convertArrayBufferToHex (arrayBuffer: ArrayBuffer, noPrefix?: boolean): string {
+export function convertArrayBufferToHex (
+  arrayBuffer: ArrayBuffer,
+  noPrefix?: boolean
+): string {
   let hex = hexlify(new Uint8Array(arrayBuffer))
   if (noPrefix) {
     hex = removeHexPrefix(hex)
@@ -125,7 +133,10 @@ export function convertNumberToUtf8 (num: number): string {
   return utf8
 }
 
-export function convertNumberToHex (num: number | string, noPrefix?: boolean): string {
+export function convertNumberToHex (
+  num: number | string,
+  noPrefix?: boolean
+): string {
   let hex = new BigNumber(num).toString(16)
   hex = sanitizeHex(hex)
   if (noPrefix) {
@@ -160,6 +171,45 @@ export function convertHexToNumber (hex: string): number {
 }
 
 // -- Misc ------------------------------------------------- //
+
+export function isMobile (): boolean {
+  let mobile: boolean = false
+
+  function hasTouchEvent (): boolean {
+    try {
+      document.createEvent('TouchEvent')
+      return true
+    } catch (e) {
+      return false
+    }
+  }
+
+  function hasMobileUserAgent (): boolean {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(
+        navigator.userAgent
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw-(n|u)|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do(c|p)o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(-|_)|g1 u|g560|gene|gf-5|g-mo|go(.w|od)|gr(ad|un)|haie|hcit|hd-(m|p|t)|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c(-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac( |-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c(-|0|1)|47|mc|nd|ri)|sgh-|shar|sie(-|m)|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel(i|m)|tim-|t-mo|to(pl|sh)|ts(70|-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(
+        navigator.userAgent.substr(0, 4)
+      )
+    ) {
+      return true
+    } else if (hasTouchEvent()) {
+      return true
+    }
+    return false
+  }
+
+  mobile = hasMobileUserAgent()
+
+  return mobile
+}
+
+export function detectEnvironment (
+  userAgent?: string
+): BrowserInfo | BotInfo | NodeInfo | null {
+  return detect(userAgent)
+}
 
 export function sanitizeHex (hex: string): string {
   hex = removeHexPrefix(hex)
@@ -211,7 +261,10 @@ export function uuid (): string {
     for (
       b = a = '';
       a++ < 36;
-      b += (a * 51) & 52 ? (a ^ 15 ? 8 ^ (Math.random() * (a ^ 20 ? 16 : 4)) : 4).toString(16) : '-'
+      b +=
+        (a * 51) & 52
+          ? (a ^ 15 ? 8 ^ (Math.random() * (a ^ 20 ? 16 : 4)) : 4).toString(16)
+          : '-'
     ) {
       // empty
     }
@@ -231,7 +284,10 @@ export const isValidAddress = (address?: string) => {
     return false
   } else if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
     return false
-  } else if (/^(0x)?[0-9a-f]{40}$/.test(address) || /^(0x)?[0-9A-F]{40}$/.test(address)) {
+  } else if (
+    /^(0x)?[0-9a-f]{40}$/.test(address) ||
+    /^(0x)?[0-9A-F]{40}$/.test(address)
+  ) {
     return true
   } else {
     return address === toChecksumAddress(address)
@@ -248,7 +304,9 @@ export function getMeta (): IClientMeta | null {
   }
 
   function getIcons (): string[] {
-    const links: HTMLCollectionOf<HTMLLinkElement> = document.getElementsByTagName('link')
+    const links: HTMLCollectionOf<
+      HTMLLinkElement
+    > = document.getElementsByTagName('link')
     const icons: string[] = []
 
     for (let i = 0; i < links.length; i++) {
@@ -265,7 +323,8 @@ export function getMeta (): IClientMeta | null {
               href.toLowerCase().indexOf('http:') === -1 &&
               href.indexOf('//') !== 0
             ) {
-              let absoluteHref: string = window.location.protocol + '//' + window.location.host
+              let absoluteHref: string =
+                window.location.protocol + '//' + window.location.host
 
               if (href.indexOf('/') === 0) {
                 absoluteHref += href
@@ -293,7 +352,9 @@ export function getMeta (): IClientMeta | null {
   }
 
   function getMetaOfAny (...args: string[]): string {
-    const metaTags: HTMLCollectionOf<HTMLMetaElement> = document.getElementsByTagName('meta')
+    const metaTags: HTMLCollectionOf<
+      HTMLMetaElement
+    > = document.getElementsByTagName('meta')
 
     for (let i = 0; i < metaTags.length; i++) {
       const tag: HTMLMetaElement = metaTags[i]
@@ -317,7 +378,12 @@ export function getMeta (): IClientMeta | null {
   }
 
   function getName (): string {
-    let name: string = getMetaOfAny('name', 'og:site_name', 'og:title', 'twitter:title')
+    let name: string = getMetaOfAny(
+      'name',
+      'og:site_name',
+      'og:title',
+      'twitter:title'
+    )
 
     if (!name) {
       name = document.title
@@ -355,13 +421,18 @@ export function getMeta (): IClientMeta | null {
 export function parseQueryString (queryString: string): any {
   const result: any = {}
 
-  const pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&')
+  const pairs = (queryString[0] === '?'
+    ? queryString.substr(1)
+    : queryString
+  ).split('&')
 
   for (let i = 0; i < pairs.length; i++) {
     const keyArr: string[] = pairs[i].match(/\w+(?==)/i) || []
     const valueArr: string[] = pairs[i].match(/=.+/i) || []
     if (keyArr[0]) {
-      result[decodeURIComponent(keyArr[0])] = decodeURIComponent(valueArr[0].substr(1))
+      result[decodeURIComponent(keyArr[0])] = decodeURIComponent(
+        valueArr[0].substr(1)
+      )
     }
   }
 
@@ -371,7 +442,8 @@ export function parseQueryString (queryString: string): any {
 export function parseWalletConnectUri (str: string): IParseURIResult {
   const pathStart: number = str.indexOf(':')
 
-  const pathEnd: number | undefined = str.indexOf('?') !== -1 ? str.indexOf('?') : undefined
+  const pathEnd: number | undefined =
+    str.indexOf('?') !== -1 ? str.indexOf('?') : undefined
 
   const protocol: string = str.substring(0, pathStart)
 
@@ -392,7 +464,8 @@ export function parseWalletConnectUri (str: string): IParseURIResult {
 
   const requiredParams: IRequiredParamsResult = parseRequiredParams(path)
 
-  const queryString: string = typeof pathEnd !== 'undefined' ? str.substr(pathEnd) : ''
+  const queryString: string =
+    typeof pathEnd !== 'undefined' ? str.substr(pathEnd) : ''
 
   function parseQueryParams (queryString: string): IQueryParamsResult {
     const result = parseQueryString(queryString)
@@ -419,7 +492,9 @@ export function parseWalletConnectUri (str: string): IParseURIResult {
 export function promisify (
   originalFn: (...args: any[]) => void,
   thisArg?: any
-): (...callArgs: any[]) => Promise<IJsonRpcResponseSuccess | IJsonRpcResponseError> {
+): (
+  ...callArgs: any[]
+) => Promise<IJsonRpcResponseSuccess | IJsonRpcResponseError> {
   const promisifiedFunction = async (
     ...callArgs: any[]
   ): Promise<IJsonRpcResponseSuccess | IJsonRpcResponseError> => {
@@ -450,14 +525,19 @@ export function parsePersonalSign (params: string[]): string[] {
   return params
 }
 
-export function parseTransactionData (txData: Partial<ITxData>): Partial<ITxData> {
+export function parseTransactionData (
+  txData: Partial<ITxData>
+): Partial<ITxData> {
   if (typeof txData.from === 'undefined' || !isValidAddress(txData.from)) {
     throw new Error(`Transaction object must include a valid 'from' value.`)
   }
 
   function parseHexValues (value: number | string) {
     let result = value
-    if (typeof value === 'number' || (typeof value === 'string' && !isEmptyString(value))) {
+    if (
+      typeof value === 'number' ||
+      (typeof value === 'string' && !isEmptyString(value))
+    ) {
       if (!isHexString(value)) {
         result = convertNumberToHex(value)
       } else if (typeof value === 'string') {
@@ -473,16 +553,22 @@ export function parseTransactionData (txData: Partial<ITxData>): Partial<ITxData
   const txDataRPC = {
     from: sanitizeHex(txData.from),
     to: typeof txData.to === 'undefined' ? '' : sanitizeHex(txData.to),
-    gasPrice: typeof txData.gasPrice === 'undefined' ? '' : parseHexValues(txData.gasPrice),
+    gasPrice:
+      typeof txData.gasPrice === 'undefined'
+        ? ''
+        : parseHexValues(txData.gasPrice),
     gasLimit:
       typeof txData.gasLimit === 'undefined'
         ? typeof txData.gas === 'undefined'
           ? ''
           : parseHexValues(txData.gas)
         : parseHexValues(txData.gasLimit),
-    value: typeof txData.value === 'undefined' ? '' : parseHexValues(txData.value),
-    nonce: typeof txData.nonce === 'undefined' ? '' : parseHexValues(txData.nonce),
-    data: typeof txData.data === 'undefined' ? '' : sanitizeHex(txData.data) || '0x'
+    value:
+      typeof txData.value === 'undefined' ? '' : parseHexValues(txData.value),
+    nonce:
+      typeof txData.nonce === 'undefined' ? '' : parseHexValues(txData.nonce),
+    data:
+      typeof txData.data === 'undefined' ? '' : sanitizeHex(txData.data) || '0x'
   }
 
   const prunable = ['gasPrice', 'gasLimit', 'value', 'nonce']
@@ -531,7 +617,9 @@ export function formatRpcError (
 
 // -- typeGuards ----------------------------------------------------------- //
 
-export function isJsonRpcSubscription (object: any): object is IJsonRpcSubscription {
+export function isJsonRpcSubscription (
+  object: any
+): object is IJsonRpcSubscription {
   return typeof object.params === 'object'
 }
 
@@ -539,11 +627,15 @@ export function isJsonRpcRequest (object: any): object is IJsonRpcRequest {
   return typeof object.method !== 'undefined'
 }
 
-export function isJsonRpcResponseSuccess (object: any): object is IJsonRpcResponseSuccess {
+export function isJsonRpcResponseSuccess (
+  object: any
+): object is IJsonRpcResponseSuccess {
   return typeof object.result !== 'undefined'
 }
 
-export function isJsonRpcResponseError (object: any): object is IJsonRpcResponseError {
+export function isJsonRpcResponseError (
+  object: any
+): object is IJsonRpcResponseError {
   return typeof object.error !== 'undefined'
 }
 
@@ -551,7 +643,9 @@ export function isInternalEvent (object: any): object is IInternalEvent {
   return typeof object.event !== 'undefined'
 }
 
-export function isWalletConnectSession (object: any): object is IWalletConnectSession {
+export function isWalletConnectSession (
+  object: any
+): object is IWalletConnectSession {
   return typeof object.bridge !== 'undefined'
 }
 
