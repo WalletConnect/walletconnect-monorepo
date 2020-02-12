791a217060463f876755a7f300b57cee190bc8cf
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-12 22:46:07 +0100

    fix qrcode-modal build

--- packages/providers/truffle-provider/webpack.config.js
+++ packages/providers/truffle-provider/webpack.config.js
@@ -12,5 +12,19 @@ module.exports = {
     library: 'WalletConnectTruffleProvider',
     umdNamedDefine: true,
     globalObject: 'this'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimize: true
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' },
+      { test: /\.svg$/, loader: 'svg-url-loader' },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] }
+    ]
   }
 }
