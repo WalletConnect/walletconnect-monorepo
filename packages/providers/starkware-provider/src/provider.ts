b86acb34daf81b6d26e13f9095563cdd186ed7f3
Pedro Gomes <pedrogomes94@gmail.com>
2020-02-22 19:35:11 +0100

    improve events

--- packages/providers/starkware-provider/src/provider.ts
+++ packages/providers/starkware-provider/src/provider.ts
@@ -1,12 +1,13 @@
 import EventEmitter from "events";
 import { payloadId } from "@walletconnect/utils";
 import WCRpcConnection from "@walletconnect/rpc-connection";
-import { timingSafeEqual } from "crypto";
 
 // -- StarkwareProvider ---------------------------------------------------- //
 
 class StarkwareProvider extends EventEmitter {
-  public connected = false;
+  private _accounts: string[] = [];
+  private _connected = false;
+
   public connection: WCRpcConnection;
 
   constructor(connection: WCRpcConnection) {
@@ -15,17 +16,29 @@ class StarkwareProvider extends EventEmitter {
   }
 
   // -- public ---------------------------------------------------------------- //
+  get accounts(): string[] {
+    return this._accounts;
+  }
+
+  set connected(value: boolean) {
+    this._connected = value;
+    if (value === true) {
+      this.emit("connect");
+    } else {
+      this.emit("close");
+    }
+  }
+
+  get connected(): boolean {
+    return this._connected;
+  }
 
   public async enable() {
     try {
       if (!this.connected) {
         await this.open();
       }
-      let { accounts } = await this.getAccounts();
-      if (!accounts.length) {
-        const registration = await this.register();
-        accounts = registration.accounts;
-      }
+      const { accounts } = await this.register();
       this.emit("enable");
       return accounts;
     } catch (err) {
@@ -48,12 +61,11 @@ class StarkwareProvider extends EventEmitter {
     return new Promise((resolve, reject) => {
       this.connection.on("close", () => {
         this.connected = false;
-        this.emit("close");
         reject();
       });
 
       this.connection.on("connect", () => {
-        this.emit("connect");
+        this.connected = true;
         resolve();
       });
 
@@ -62,14 +74,14 @@ class StarkwareProvider extends EventEmitter {
   }
 
   public close() {
-    this.connection.close();
     this.connected = false;
+    this.connection.close();
   }
 
   public async getAccounts() {
     await this.send("stark_accounts");
     // 1. send stark_accounts with array of starkKeys
-    // 2. wallet returns array
+    // 2. wallet returns accounts array
     const accounts: string[] = [];
     return { accounts };
   }
@@ -80,7 +92,7 @@ class StarkwareProvider extends EventEmitter {
     // 2. wallet generates starkKey (if not present)
     // 3. wallet signs and ETH message of the hash of ethKey and hashKey
     // 4. wallet sends transaction of starkKey and registration signature to smart contract
-    // 5. wallet returns starkKey and transaction hash
+    // 5. wallet returns accounts array and transaction hash
     const accounts: string[] = [];
     const txhash = "";
     return { accounts, txhash };
