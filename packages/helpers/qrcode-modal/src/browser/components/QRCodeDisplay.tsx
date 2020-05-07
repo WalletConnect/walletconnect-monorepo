527143a4f56294a160dfa9e806ac8d58adb97d58
Pedro Gomes <pedrogomes94@gmail.com>
2020-05-07 14:41:30 +0200

    migrate browser components to preact

---
+++ packages/helpers/qrcode-modal/src/browser/components/QRCodeDisplay.tsx
@@ -0,0 +1,31 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as React from "react";
+import * as qrImage from "qr-image";
+import { WALLETCONNECT_CTA_TEXT_ID } from "../constants";
+
+function formatQRCodeImage(data: string) {
+  let result = "";
+  const dataString = qrImage.imageSync(data, { type: "svg" });
+  if (typeof dataString === "string") {
+    result = dataString.replace("<svg", `<svg className="walletconnect-qrcode__image"`);
+  }
+  return result;
+}
+
+interface QRCodeDisplayProps {
+  uri: string;
+}
+
+function QRCodeDisplay(props: QRCodeDisplayProps) {
+  const svg = formatQRCodeImage(props.uri);
+  return (
+    <div>
+      <p id={WALLETCONNECT_CTA_TEXT_ID} className="walletconnect-qrcode__text">
+        {"Scan QR code with a WalletConnect-compatible wallet"}
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: svg }}></div>
+    </div>
+  );
+}
+
+export default QRCodeDisplay;
