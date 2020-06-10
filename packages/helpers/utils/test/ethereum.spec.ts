af24b1746a3f3102687f496cbbc190a42c1ab064
Pedro Gomes <pedrogomes94@gmail.com>
2020-06-10 21:10:11 +0200

    add toChecksumAddress test

---
+++ packages/helpers/utils/test/ethereum.spec.ts
@@ -0,0 +1,20 @@
+import { getAddress } from "@ethersproject/address";
+
+import * as ethereumUtils from "../src/ethereum";
+
+const TEST_ADDRESS = "0x9b7b2B4f7a391b6F14A81221AE0920A9735B67Fb";
+
+describe("Ethereum Utils", () => {
+  it("toChecksumAddress", async () => {
+    const input = TEST_ADDRESS;
+    const expected = getAddress(input);
+    const result = ethereumUtils.toChecksumAddress(input);
+    expect(result).toEqual(expected);
+  });
+  it("isValidAddress", async () => {
+    const input = TEST_ADDRESS;
+    const expected = true;
+    const result = ethereumUtils.isValidAddress(input);
+    expect(result).toEqual(expected);
+  });
+});
