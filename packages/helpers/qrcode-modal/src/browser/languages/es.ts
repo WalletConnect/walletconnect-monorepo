46544df4d085d4eed62037be8c843fcf24045037
Pedro Gomes <pedrogomes94@gmail.com>
2020-06-27 12:51:20 +0200

    update es.t.s

--- packages/helpers/qrcode-modal/src/browser/languages/es.ts
+++ packages/helpers/qrcode-modal/src/browser/languages/es.ts
@@ -1,8 +1,8 @@
 export default {
-  choose_preferred_wallet: "Elige tu billetra preferida",
-  connect_mobile_wallet: "Conectar a billetra móvil",
-  scan_qrcode_with_wallet: "Escanea el código QR con una billetra compatible con WalletConnect",
-  return_to_mobile_options: "Regresar a las opciones de billetra móvil",
-  view_qrcode_option: "Ver código QR en vez",
+  choose_preferred_wallet: "Elige tu billetera preferida",
+  connect_mobile_wallet: "Conectar a billetera móvil",
+  scan_qrcode_with_wallet: "Escanea el código QR con una billetera compatible con WalletConnect",
+  return_to_mobile_options: "Regresar a las opciones de billetera móvil",
+  view_qrcode_option: "Ver código QR",
   connect: "Conectar",
 };
