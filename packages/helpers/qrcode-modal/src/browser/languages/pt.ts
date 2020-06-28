6b1c05084224234c0be3219746a42f5a590b7800
Pedro Gomes <pedrogomes94@gmail.com>
2020-06-28 14:11:28 +0200

    add Portuguese translation

---
+++ packages/helpers/qrcode-modal/src/browser/languages/pt.ts
@@ -0,0 +1,8 @@
+export default {
+  choose_preferred_wallet: "Escolha sua carteira preferida",
+  connect_mobile_wallet: "Conetar-se à carteira móvel",
+  scan_qrcode_with_wallet: "Digitalize o código QR com uma carteira compatível com WalletConnect",
+  return_to_mobile_options: "Regressar às opções de carteira móvel",
+  view_qrcode_option: "Ver código QR",
+  connect: "Conectar",
+};
