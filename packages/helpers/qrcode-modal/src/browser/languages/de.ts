966ac45426915fe29f4ea254ace0285ba06c908a
Pedro Gomes <pedrogomes94@gmail.com>
2020-06-28 20:08:45 +0200

    update de.ts

--- packages/helpers/qrcode-modal/src/browser/languages/de.ts
+++ packages/helpers/qrcode-modal/src/browser/languages/de.ts
@@ -1,8 +1,8 @@
 export default {
-  choose_preferred_wallet: "Wähle bevorzugten Wallet",
+  choose_preferred_wallet: "Wähle bevorzugte Wallet",
   connect_mobile_wallet: "Verbinde mit Mobile Wallet",
-  scan_qrcode_with_wallet: "Scanne den QR Code mit einem WalletConnect-kompatiblen Wallet",
-  return_to_mobile_options: "Zurück zur Mobile Wallet Option",
-  view_qrcode_option: "Zeige QR code",
+  scan_qrcode_with_wallet: "Scanne den QR-code mit einer WalletConnect kompatiblen Wallet",
+  return_to_mobile_options: "Zurück zur Wallet-Anbieter Übersicht",
+  view_qrcode_option: "QR-code Anzeigen",
   connect: "Verbinden",
 };
