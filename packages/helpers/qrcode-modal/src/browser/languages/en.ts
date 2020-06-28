ce3c529b88882c65afe2f0cf96ae320f10d0f20c
Pedro Gomes <pedrogomes94@gmail.com>
2020-06-28 13:42:52 +0200

    update English text

--- packages/helpers/qrcode-modal/src/browser/languages/en.ts
+++ packages/helpers/qrcode-modal/src/browser/languages/en.ts
@@ -3,6 +3,6 @@ export default {
   connect_mobile_wallet: "Connect to Mobile Wallet",
   scan_qrcode_with_wallet: "Scan QR code with a WalletConnect-compatible wallet",
   return_to_mobile_options: "Return to mobile wallet options",
-  view_qrcode_option: "View QR code instead",
+  view_qrcode_option: "View QR code",
   connect: "Connect",
 };
