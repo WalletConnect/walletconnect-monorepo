39f1eee35d1ade8bb82fa74faf9127fc986da656
Pedro Gomes <pedrogomes94@gmail.com>
2020-06-28 13:42:22 +0200

    add Chinese and German translations

---
+++ packages/helpers/qrcode-modal/src/browser/languages/de.ts
@@ -0,0 +1,8 @@
+export default {
+  choose_preferred_wallet: "Wähle bevorzugten Wallet",
+  connect_mobile_wallet: "Verbinde mit Mobile Wallet",
+  scan_qrcode_with_wallet: "Scanne den QR Code mit einem WalletConnect-kompatiblen Wallet",
+  return_to_mobile_options: "Zurück zur Mobile Wallet Option",
+  view_qrcode_option: "Zeige QR code",
+  connect: "Verbinden",
+};
