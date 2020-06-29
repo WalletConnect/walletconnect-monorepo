bd945cc6965101303b38e6619772815f9c0d2d79
Pedro Gomes <pedrogomes94@gmail.com>
2020-06-29 16:05:06 +0200

    add korean

--- packages/helpers/qrcode-modal/src/browser/languages/index.ts
+++ packages/helpers/qrcode-modal/src/browser/languages/index.ts
@@ -1,4 +1,9 @@
+import de from "./de";
 import en from "./en";
 import es from "./es";
+import fr from "./fr";
+import ko from "./ko";
+import pt from "./pt";
+import zh from "./zh";
 
-export default { en, es };
+export default { de, en, es, fr, ko, pt, zh };
---
+++ packages/helpers/qrcode-modal/src/browser/languages/ko.ts
@@ -0,0 +1,8 @@
+export default {
+  choose_preferred_wallet: "원하는 지갑을 선택하세요",
+  connect_mobile_wallet: "모바일 지갑과 연결",
+  scan_qrcode_with_wallet: "WalletConnect 지원 지갑에서 QR코드를 스캔하세요",
+  return_to_mobile_options: "모바일 지갑 옵션으로 돌아가기",
+  view_qrcode_option: "QR 코드 보기",
+  connect: "연결",
+};
