b8d1f87824a4ee708ba76c5f471a7c4c8b7c2ce3
Pedro Gomes <pedrogomes94@gmail.com>
2020-07-20 10:06:01 +0200

    add to prebuild

--- packages/helpers/qrcode-modal/src/browser/components/DeepLinkDisplay.tsx
+++ packages/helpers/qrcode-modal/src/browser/components/DeepLinkDisplay.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
-import MobileRegistry from "@walletconnect/mobile-registry";
 import { IMobileRegistryEntry } from "@walletconnect/types";
 import { isIOS, deeplinkChoiceKey, setLocal } from "@walletconnect/utils";
 
 import { DEFAULT_BUTTON_COLOR, WALLETCONNECT_CTA_TEXT_ID } from "../constants";
+
+import { MOBILE_REGISTRY } from "../assets/registry";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ConnectButton from "./ConnectButton";
@@ -45,7 +46,7 @@ function DeepLinkDisplay(props: DeepLinkDisplayProps) {
       </p>
       <div className={`walletconnect-connect__buttons__wrapper${!ios && "__android"}`}>
         {ios ? (
-          MobileRegistry.map((entry: IMobileRegistryEntry) => {
+          MOBILE_REGISTRY.map((entry: IMobileRegistryEntry) => {
             const { color, name, logo } = entry;
             const href = formatIOSDeepLink(props.uri, entry);
             const handleClickIOS = React.useCallback(() => {
