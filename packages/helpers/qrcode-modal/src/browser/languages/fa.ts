b7c3c5e42d9983d771aa946dc6fa95b047107f58
Shayan Eskandari <shayanbahal@gmail.com>
2020-07-28 10:42:17 -0400

    Add Farsi (Persian) translation

---
+++ packages/helpers/qrcode-modal/src/browser/languages/fa.ts
@@ -0,0 +1,8 @@
+export default {
+  choose_preferred_wallet: "کیف پول مورد نظر خود را انتخاب کنید",
+  connect_mobile_wallet: "به کیف پول موبایل وصل شوید",
+  scan_qrcode_with_wallet: "کد QR را با یک کیف پول سازگار با WalletConnect اسکن کنید",
+  return_to_mobile_options: "به گزینه های کیف پول موبایل برگردید",
+  view_qrcode_option: "مشاهده کد QR",
+  connect: "اتصال",
+};
