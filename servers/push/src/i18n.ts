fed0cad30129556403a837f5787e7c6fc85f9163
Pedro Gomes <pedrogomes94@gmail.com>
2020-11-30 16:34:27 +0100

    push server

---
+++ servers/push/src/i18n.ts
@@ -0,0 +1,449 @@
+import { LanguageTranslation } from "./types";
+
+const data: LanguageTranslation[] = [
+  {
+    iso639_1: "af",
+    name: "Afrikaans",
+    strings: ["Nuwe versoek", "Nuwe versoek van PEER_NAME"],
+  },
+  {
+    iso639_1: "ar",
+    name: "Arabic",
+    strings: ["طلب جديد", "طلب جديد من PEER_NAME"],
+  },
+  {
+    iso639_1: "az",
+    name: "Azerbaijani",
+    strings: ["Yeni tələb", "PEER_NAME dən yeni sorğu"],
+  },
+  {
+    iso639_1: "be",
+    name: "Belarusian",
+    strings: ["новы запыт", "Новы запыт ад PEER_NAME"],
+  },
+  {
+    iso639_1: "bg",
+    name: "Bulgarian",
+    strings: ["Нова заявка", "Нова заявка от PEER_NAME"],
+  },
+  {
+    iso639_1: "bn",
+    name: "Bengali",
+    strings: ["নতুন অনুরোধ", "PEER_NAME থেকে নতুন অনুরোধ"],
+  },
+  {
+    iso639_1: "bs",
+    name: "Bosnian",
+    strings: ["Novi zahtjev", "Novi zahtjev od korisnika PEER_NAME"],
+  },
+  {
+    iso639_1: "ca",
+    name: "Catalan",
+    strings: ["Nova sol·licitud", "Nova sol·licitud de PEER_NAME"],
+  },
+  {
+    iso639_1: "cs",
+    name: "Czech",
+    strings: ["Nová žádost", "Nová žádost od PEER_NAME"],
+  },
+  {
+    iso639_1: "cy",
+    name: "Welsh",
+    strings: ["Cais Newydd", "Cais Newydd gan PEER_NAME"],
+  },
+  {
+    iso639_1: "da",
+    name: "Danish",
+    strings: ["Ny anmodning", "Ny anmodning fra PEER_NAME"],
+  },
+  {
+    iso639_1: "de",
+    name: "German",
+    strings: ["Neue Anfrage", "Neue Anfrage von PEER_NAME"],
+  },
+  {
+    iso639_1: "el",
+    name: "Greek",
+    strings: ["Νέα αίτηση", "Νέα αίτηση από PEER_NAME"],
+  },
+  {
+    iso639_1: "en",
+    name: "English",
+    strings: ["New Request", "New Request from PEER_NAME"],
+  },
+  {
+    iso639_1: "es",
+    name: "Spanish",
+    strings: ["Nueva solicitud", "Nueva solicitud de PEER_NAME"],
+  },
+  {
+    iso639_1: "et",
+    name: "Estonian",
+    strings: ["Uus päring", "Uus PEER_NAME päring"],
+  },
+  {
+    iso639_1: "eu",
+    name: "Basque",
+    strings: ["Eskaera berria", "PEER_NAME ren eskaera berria"],
+  },
+  {
+    iso639_1: "fa",
+    name: "Persian",
+    strings: ["درخواست جدید", "درخواست جدید از PEER_NAME"],
+  },
+  {
+    iso639_1: "fi",
+    name: "Finnish",
+    strings: ["Uusi pyyntö", "Uusi pyyntö käyttäjältä PEER_NAME"],
+  },
+  {
+    iso639_1: "fr",
+    name: "French",
+    strings: ["Nouvelle requête", "Nouvelle demande de PEER_NAME"],
+  },
+  {
+    iso639_1: "ga",
+    name: "Irish",
+    strings: ["Iarratas Nua", "Iarratas Nua ó PEER_NAME"],
+  },
+  {
+    iso639_1: "gl",
+    name: "Galician",
+    strings: ["Nova solicitude", "Nova solicitude de PEER_NAME"],
+  },
+  {
+    iso639_1: "gu",
+    name: "Gujarati",
+    strings: ["નવી વિનંતી", "PEER_NAME તરફથી નવી વિનંતી"],
+  },
+  {
+    iso639_1: "ha",
+    name: "Hausa",
+    strings: ["Sabon Buƙatar", "Sabuwar Tambaya daga PEER_NAME"],
+  },
+  {
+    iso639_1: "he",
+    name: "Hebrew",
+    strings: ["בקשה חדשה", "בקשה חדשה מ PEER_NAME"],
+  },
+  {
+    iso639_1: "hi",
+    name: "Hindi",
+    strings: ["नया अनुरोध", "PEER_NAME से नया अनुरोध"],
+  },
+  {
+    iso639_1: "hr",
+    name: "Croatian",
+    strings: ["Novi zahtjev", "Novi zahtjev korisnika PEER_NAME"],
+  },
+  {
+    iso639_1: "ht",
+    name: "Haitian",
+    strings: ["Nouvo Demand", "Nouvo Demann nan PEER_NAME"],
+  },
+  {
+    iso639_1: "hu",
+    name: "Hungarian",
+    strings: ["Új kérés", "Új kérés az PEER_NAME től"],
+  },
+  {
+    iso639_1: "hy",
+    name: "Armenian",
+    strings: ["Նոր հայտ", "Նոր պահանջ PEER_NAME ից"],
+  },
+  {
+    iso639_1: "id",
+    name: "Indonesian",
+    strings: ["Permintaan baru", "Permintaan Baru dari PEER_NAME"],
+  },
+  {
+    iso639_1: "ig",
+    name: "Igbo",
+    strings: ["Arịrịọ ọhụrụ", "Arịrịọ ọhụrụ si PEER_NAME"],
+  },
+  {
+    iso639_1: "is",
+    name: "Icelandic",
+    strings: ["Ný beiðni", "Ný beiðni frá PEER_NAME"],
+  },
+  {
+    iso639_1: "it",
+    name: "Italian",
+    strings: ["Nuova richiesta", "Nuova richiesta da PEER_NAME"],
+  },
+  {
+    iso639_1: "ja",
+    name: "Japanese",
+    strings: ["新しい依頼", "PEER_NAMEからの新しい要求"],
+  },
+  {
+    iso639_1: "jv",
+    name: "Javanese",
+    strings: ["Request anyar", "Request anyar saka PEER_NAME"],
+  },
+  {
+    iso639_1: "ka",
+    name: "Georgian",
+    strings: ["ახალი მოთხოვნა", "ახალი მოთხოვნა PEER_NAME დან"],
+  },
+  {
+    iso639_1: "kk",
+    name: "Kazakh",
+    strings: ["Жаңа сұраныс", "PEER_NAME жаңа сұраныс"],
+  },
+  {
+    iso639_1: "km",
+    name: "Khmer",
+    strings: ["សំណើថ្មី", "សំណើថ្មីពី PEER_NAME"],
+  },
+  {
+    iso639_1: "kn",
+    name: "Kannada",
+    strings: ["ಹೊಸ ವಿನಂತಿ", "PEER_NAME ನಿಂದ ಹೊಸ ವಿನಂತಿ"],
+  },
+  {
+    iso639_1: "ko",
+    name: "Korean",
+    strings: ["새로운 요청", "PEER_NAME 님의 새 요청"],
+  },
+  {
+    iso639_1: "lo",
+    name: "Lao",
+    strings: ["ຄໍາຮ້ອງຂໍໃຫມ່", "ການຮ້ອງຂໍໃຫມ່ຈາກ PEER_NAME"],
+  },
+  {
+    iso639_1: "lt",
+    name: "Lithuanian",
+    strings: ["Naujas prašymas", "Naujas prašymas iš „PEER_NAME“"],
+  },
+  {
+    iso639_1: "lv",
+    name: "Latvian",
+    strings: ["Jauns pieprasījums", "Jauns pieprasījums no PEER_NAME"],
+  },
+  {
+    iso639_1: "mi",
+    name: "Maori",
+    strings: ["Nga tono hou", "Nga tono hou mai i PEER_NAME"],
+  },
+  {
+    iso639_1: "mk",
+    name: "Macedonian",
+    strings: ["Ново барање", "Ново барање од PEER_NAME"],
+  },
+  {
+    iso639_1: "ml",
+    name: "Malayalam",
+    strings: ["പുതിയ അഭ്യർത്ഥന", "PEER_NAME ൽ നിന്നുള്ള പുതിയ അഭ്യർത്ഥന"],
+  },
+  {
+    iso639_1: "mn",
+    name: "Mongolian",
+    strings: ["Шинэ хүсэлт", "PEER_NAME ийн шинэ хүсэлт"],
+  },
+  {
+    iso639_1: "mr",
+    name: "Marathi",
+    strings: ["नवीन विनंती", "PEER_NAME कडून नवीन विनंती"],
+  },
+  {
+    iso639_1: "ms",
+    name: "Malay",
+    strings: ["Permintaan baru", "Permintaan baharu dari PEER_NAME"],
+  },
+  {
+    iso639_1: "mt",
+    name: "Maltese",
+    strings: ["Talba Ġdida", "Talba Ġdida minn PEER_NAME"],
+  },
+  {
+    iso639_1: "ne",
+    name: "Nepali",
+    strings: ["नयाँ अनुरोध", "PEER_NAME बाट नयाँ अनुरोध"],
+  },
+  {
+    iso639_1: "nl",
+    name: "Dutch",
+    strings: ["Nieuw verzoek", "Nieuw verzoek van PEER_NAME"],
+  },
+  {
+    iso639_1: "no",
+    name: "Norwegian",
+    strings: ["Ny forespørsel", "Ny forespørsel fra PEER_NAME"],
+  },
+  {
+    iso639_1: "ny",
+    name: "Chichewa",
+    strings: ["Funso Latsopano", "Pempho Latsopano la PEER_NAME"],
+  },
+  {
+    iso639_1: "pa",
+    name: "Punjabi",
+    strings: ["ਨਵਾਂ ਬੇਨਤੀ", "PEER_NAME ਤੋਂ ਨਵੀਂ ਬੇਨਤੀ"],
+  },
+  {
+    iso639_1: "pl",
+    name: "Polish",
+    strings: ["Nowe żądanie", "Nowe żądanie od PEER_NAME"],
+  },
+  {
+    iso639_1: "pt",
+    name: "Portuguese",
+    strings: ["Novo pedido", "Novo pedido de PEER_NAME"],
+  },
+  {
+    iso639_1: "ro",
+    name: "Romanian",
+    strings: ["Cerere nouă", "Nouă solicitare de la PEER_NAME"],
+  },
+  {
+    iso639_1: "ru",
+    name: "Russian",
+    strings: ["Новый запрос", "Новый запрос от PEER_NAME"],
+  },
+  {
+    iso639_1: "si",
+    name: "Sinhala",
+    strings: ["නව ඉල්ලීම", "PEER_NAME වෙතින් අලුත් ඉල්ලීමක්"],
+  },
+  {
+    iso639_1: "sk",
+    name: "Slovak",
+    strings: ["Nová požiadavka", "Nová žiadosť od PEER_NAME"],
+  },
+  {
+    iso639_1: "sl",
+    name: "Slovenian",
+    strings: ["Nova zahteva", "Nova zahteva osebe PEER_NAME"],
+  },
+  {
+    iso639_1: "so",
+    name: "Somali",
+    strings: ["Codsi Cusub", "Codsi Cusub laga bilaabo PEER_NAME"],
+  },
+  {
+    iso639_1: "sq",
+    name: "Albanian",
+    strings: ["Kërkesë e re", "Kërkesë e re nga PEER_NAME"],
+  },
+  {
+    iso639_1: "sr",
+    name: "Serbian",
+    strings: ["Нев Рекуест", "Нови захтев од PEER_NAME"],
+  },
+  {
+    iso639_1: "st",
+    name: "Sesotho",
+    strings: ["Kopo e ncha", "Kopo e ncha ho PEER_NAME"],
+  },
+  {
+    iso639_1: "su",
+    name: "Sundanese",
+    strings: ["Request anyar", "Request Anyar ti PEER_NAME"],
+  },
+  {
+    iso639_1: "sv",
+    name: "Swedish",
+    strings: ["Ny förfrågan", "Ny begäran från PEER_NAME"],
+  },
+  {
+    iso639_1: "sw",
+    name: "Swahili",
+    strings: ["Ombi mpya", "Ombi mpya kutoka PEER_NAME"],
+  },
+  {
+    iso639_1: "ta",
+    name: "Tamil",
+    strings: ["புதிய கோரிக்கை", "PEER_NAME இலிருந்து புதிய கோரிக்கை"],
+  },
+  {
+    iso639_1: "te",
+    name: "Telugu",
+    strings: ["క్రొత్త అభ్యర్థన", "PEER_NAME నుండి క్రొత్త అభ్యర్థన"],
+  },
+  {
+    iso639_1: "tg",
+    name: "Tajik",
+    strings: ["Дархости нав", "Талаботи нав аз PEER_NAME"],
+  },
+  {
+    iso639_1: "th",
+    name: "Thai",
+    strings: ["คำขอใหม่", "คำขอใหม่จาก PEER_NAME"],
+  },
+  {
+    iso639_1: "tl",
+    name: "Filipino",
+    strings: ["Bagong Kahilingan", "Bagong Hiling mula sa PEER_NAME"],
+  },
+  {
+    iso639_1: "tr",
+    name: "Turkish",
+    strings: ["Yeni istek", "PEER_NAME tan Yeni İstek"],
+  },
+  {
+    iso639_1: "uk",
+    name: "Ukrainian",
+    strings: ["Новий запит", "Новий запит від PEER_NAME"],
+  },
+  {
+    iso639_1: "ur",
+    name: "Urdu",
+    strings: ["نئی درخواست", "PEER_NAME سے نئی درخواست"],
+  },
+  {
+    iso639_1: "uz",
+    name: "Uzbek",
+    strings: ["Yangi so rov", "PEER_NAME dan yangi so rov"],
+  },
+  {
+    iso639_1: "vi",
+    name: "Vietnamese",
+    strings: ["Yêu cầu mới", "Yêu cầu mới từ PEER_NAME"],
+  },
+  {
+    iso639_1: "yo",
+    name: "Yoruba",
+    strings: ["Ibere ​​Titun", "Ibere ​​titun lati PEER_NAME"],
+  },
+  {
+    iso639_1: "zh",
+    name: "Chinese",
+    strings: ["新请求", "来自PEER_NAME的新请求"],
+  },
+  {
+    iso639_1: "zu",
+    name: "Zulu",
+    strings: ["Isicelo esisha", "Isicelo esisha ku PEER_NAME"],
+  },
+];
+
+export function getLanguage(code: string): LanguageTranslation | undefined {
+  let language: LanguageTranslation | undefined;
+  const array = Object.values(data).filter(lang => lang.iso639_1 === code);
+  if (array && array.length) {
+    language = array[0];
+  }
+  return language;
+}
+
+export function formatMessage(peerName: string, code: string): string {
+  let result = "New Request";
+  if (peerName) {
+    result = `New Request from ${peerName}`;
+    if (code) {
+      const language = getLanguage(code);
+      if (language) {
+        result = language.strings[1].replace("PEER_NAME", peerName);
+      }
+    }
+  } else {
+    if (code) {
+      const language = getLanguage(code);
+      if (language) {
+        result = language.strings[0];
+      }
+    }
+  }
+  return result;
+}
