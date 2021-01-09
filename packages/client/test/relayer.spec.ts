8bf2cdc4463a11c405dd7f1dcae427be5630ae1f
Pedro Gomes <pedrogomes94@gmail.com>
2021-01-09 18:42:32 +0100

    tweak tests

---
+++ packages/client/test/relayer.spec.ts
@@ -0,0 +1,33 @@
+import "mocha";
+
+import { expect, testApproveSession, setupClientsForTesting } from "./shared";
+
+describe("Relayer", function() {
+  this.timeout(30_000);
+  it("A pings B after A socket reconnects", async () => {
+    // setup
+    const { setup, clients } = await setupClientsForTesting();
+    // connect
+    const topic = await testApproveSession(setup, clients);
+    // ping
+    await clients.a.session.ping(topic);
+    // disconnect
+    await clients.a.relayer.provider.connection.close();
+    expect(clients.a.relayer.provider.connection.connected).to.be.false;
+    // ping
+    await clients.a.session.ping(topic);
+  });
+  it("A pings B after B socket reconnects", async () => {
+    // setup
+    const { setup, clients } = await setupClientsForTesting();
+    // connect
+    const topic = await testApproveSession(setup, clients);
+    // ping
+    await clients.a.session.ping(topic);
+    // disconnect
+    await clients.b.relayer.provider.connection.close();
+    expect(clients.b.relayer.provider.connection.connected).to.be.false;
+    // ping
+    await clients.a.session.ping(topic);
+  });
+});
