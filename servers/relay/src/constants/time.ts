63ac4f2ea4aec4df8aa9573b2b2fcb4bb4181a54
Pedro Gomes <pedrogomes94@gmail.com>
2021-01-29 15:02:08 +0100

    standardize all time constants

---
+++ servers/relay/src/constants/time.ts
@@ -0,0 +1,11 @@
+export const THIRTY_DAYS = 2592000;
+
+export const SEVEN_DAYS = 604800;
+
+export const ONE_DAY = 86400;
+
+export const SIX_HOURS = 21600;
+
+export const TEN_SECONDS = 10;
+
+export const FIVE_SECONDS = 5;
