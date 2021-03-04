f37e021848f1221c2fa729357c0f08e3fc6e7ff3
Pedro Gomes <pedrogomes94@gmail.com>
2021-03-04 18:43:33 +0100

    add relay modes

--- servers/relay/src/utils/legacy.ts
+++
@@ -1,5 +0,0 @@
-import { LegacySocketMessage } from "../types";
-
-export function isLegacySocketMessage(payload: any): payload is LegacySocketMessage {
-  return "topic" in payload && "type" in payload && "payload" in payload;
-}
