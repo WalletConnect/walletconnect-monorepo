6b8c47c3f6a5a0496e3a67ce127dcb6c7d02af5d
Pedro Gomes <pedrogomes94@gmail.com>
2021-03-04 18:25:09 +0100

    waku prefix

--- packages/client/src/constants/relayer.ts
+++ packages/client/src/constants/relayer.ts
@@ -2,7 +2,7 @@ import { SIX_HOURS } from "./time";
 
 export const RELAYER_DEFAULT_PUBLISH_TTL = SIX_HOURS;
 
-export const RELAYER_DEFAULT_PROTOCOL = "bridge";
+export const RELAYER_DEFAULT_PROTOCOL = "waku";
 
 export const RELAYER_DEFAULT_RPC_URL = "wss://relay.walletconnect.org";
 
