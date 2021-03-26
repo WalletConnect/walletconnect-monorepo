e6eedb11d260446a49e723021f23a72562068e1a
Sebastian Bolanos <sebasheston@protonmail.com>
2021-03-26 22:01:02 +0000

    Add test for two waku nodes

--- servers/relay/src/waku.ts
+++ servers/relay/src/waku.ts
@@ -33,16 +33,20 @@ export class WakuService extends HttpConnection {
 
   constructor(public logger: Logger, nodeUrl: string) {
     super(nodeUrl);
-    this.logger = generateChildLogger(logger, this.context);
+    this.logger = generateChildLogger(logger, `${this.context}@${nodeUrl}`);
     this.initialize();
   }
 
   private initialize(): void {
     this.logger.trace(`Initialized`);
     this.open();
-    this.on("payload", (payload: JsonRpcPayload) =>
-      this.events.emit(payload.id.toString(), payload),
-    );
+    this.on("payload", (payload: JsonRpcPayload) => {
+      if (isJsonRpcError(payload)) {
+        this.logger.error(payload.error);
+      }
+      this.logger.trace({ method: "New Payload", payload });
+      this.events.emit(payload.id.toString(), payload);
+    });
   }
 
   public async postMessage(topic: string, message: string) {
@@ -59,18 +63,23 @@ export class WakuService extends HttpConnection {
   }
   public getMessages(topics: Array<string>): Promise<Array<WakuMessage>> {
     let payload = formatJsonRpcRequest("get_waku_v2_relay_v1_messages", topics);
+    this.logger.debug("Getting Messages");
+    this.logger.trace({ type: "method", method: "getMessages", topics });
     this.send(payload);
     return new Promise((resolve, reject) => {
       this.on(payload.id.toString(), (response: JsonRpcResponse) => {
         if (isJsonRpcError(response)) {
+          this.logger.error(response.error);
           reject(response.error);
         }
         resolve((response as JsonRpcResult<Array<WakuMessage>>).result);
       });
     });
   }
-  public subscribe(topics: Array<string>) {
-    let payload = formatJsonRpcRequest("post_waku_v2_relay_v1_subscriptions", topics);
+  public async subscribe(topics: Array<string>) {
+    let payload = formatJsonRpcRequest("post_waku_v2_relay_v1_subscriptions", [topics]);
+    this.logger.debug("Subscribing to Waku Topic");
+    this.logger.trace({ type: "method", method: "subscribe", topics });
     this.send(payload);
   }
   public unsubscribe(topic: string) {
--- servers/relay/test/waku.spec.ts
+++ servers/relay/test/waku.spec.ts
@@ -4,32 +4,30 @@ import pino from "pino";
 import { getDefaultLoggerOptions } from "@pedrouid/pino-utils";
 import { WakuService, WakuMessage } from "../src/waku";
 import { arrayToHex } from "enc-utils";
+import { generateRandomBytes32 } from "../src/utils";
 
 import { TEST_WAKU_URL } from "./shared";
 
-let testTopic = "my_topic_1";
-let testMessage = "1a2b3c4d5e6e";
+let testMessage = "48656c6c6f20576f726c6421";
 
-describe.only("Waku", () => {
+describe("Waku", () => {
   // We can use a single waku node with two WakuService
   let wakuOne: WakuService;
   let wakuTwo: WakuService;
+  let testTopic: string;
   before(() => {
-    wakuTwo = new WakuService(pino(getDefaultLoggerOptions({ level: "trace" })), TEST_WAKU_URL);
     wakuOne = new WakuService(pino(getDefaultLoggerOptions({ level: "trace" })), TEST_WAKU_URL);
+    wakuTwo = new WakuService(
+      pino(getDefaultLoggerOptions({ level: "trace" })),
+      TEST_WAKU_URL.replace("8546", "8547"),
+    );
   });
-  it.only("It has waku peers", async () => {
+  beforeEach(() => {
+    testTopic = generateRandomBytes32();
+  });
+  it("Waku node has peers", async () => {
     let peers = await wakuOne.getPeers();
     expect(peers.length).to.be.greaterThan(0);
-  });
-  it("Waku peers should have structure WakuPeers", async () => {
-    let peers = await wakuOne.getPeers();
-  });
-  it("Posts a message to waku", async () => {
-    wakuOne.postMessage(testTopic, testMessage);
-  });
-  it("Subscribe to messages", async () => {
-    wakuOne.subscribe([testTopic]);
   });
   it("It polls for messages", async () => {});
   it.only("Receives a message from waku", async () => {
@@ -38,5 +36,18 @@ describe.only("Waku", () => {
     let messages = await wakuOne.getMessages([testTopic]);
     expect(messages.length).to.greaterThan(0);
     expect(arrayToHex(messages[0].payload)).to.equal(testMessage);
+  });
+  it.only("Multiple waku nodes", function(done) {
+    this.timeout(5000);
+    wakuTwo.subscribe([testTopic]);
+    setTimeout(async () => {
+      await wakuOne.postMessage(testTopic, testMessage);
+    }, 500);
+    setTimeout(async () => {
+      let messages = await wakuTwo.getMessages([testTopic]);
+      expect(messages.length).to.be.greaterThan(0);
+      expect(arrayToHex(messages[0].payload)).to.equal(testMessage);
+      done();
+    }, 1000);
   });
 });
