53c09fa4014cffb527d3694e723d3548481b7d7c
Pedro Gomes <pedrogomes94@gmail.com>
2021-04-24 17:22:20 +0200

    webpack

--- packages/ethereum-provider/webpack.config.js
+++ packages/ethereum-provider/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist", "umd"),
     filename: "[name].min.js",
     libraryTarget: "umd",
-    library: "WalletConnectSignerConnection",
+    library: "WalletConnectEthereumProvider",
     umdNamedDefine: true,
     globalObject: "this",
   },
