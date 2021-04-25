882d01eef4ea5f029537137c80673043f412e4ca
Robertosnap <robertosnap@pm.me>
2021-04-26 00:09:18 +0200

    more package lock

--- packages/ethereum-provider/test/index.spec.ts
+++ packages/ethereum-provider/test/index.spec.ts
@@ -1,5 +1,9 @@
+import "mocha";
+import EthereumProvider from "./../src/index";
+import { TestNetwork } from "ethereum-test-network";
 describe("@walletconnect/ethereum-provider", () => {
   it("needs tests", () => {
     // needs tests
+    console.log("Lol");
   });
 });
