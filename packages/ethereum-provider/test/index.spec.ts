8eaa3fbbc58d4462e3fa254bf1114a60f88f2abe
Robertosnap <robertosnap@pm.me>
2021-04-26 01:00:44 +0200

    Added ethreum testnetwork and started test

--- packages/ethereum-provider/test/index.spec.ts
+++ packages/ethereum-provider/test/index.spec.ts
@@ -1,7 +1,10 @@
 import "mocha";
 import EthereumProvider from "./../src/index";
+import { SignerConnection } from "@walletconnect/signer-connection";
 import { TestNetwork } from "ethereum-test-network";
 import { expect } from "chai";
+import { Client, CLIENT_EVENTS } from "@walletconnect/client";
+import { IClient, RequestEvent, SessionTypes } from "@walletconnect/types";
 
 const CHAIN_ID = 123;
 const PORT = 8545;
@@ -12,6 +15,31 @@ const DEFAULT_GENESIS_ACCOUNTS = [
     privateKey: "0xa3dac6ca0b1c61f5f0a0b3a0acf93c9a52fd94e8e33d243d3b3a8b8c5dc37f0b", // 0xaaE062157B53077da1414ec3579b4CBdF7a4116f
   },
 ];
+
+export const TEST_RELAY_URL = process.env.TEST_RELAY_URL
+  ? process.env.TEST_RELAY_URL
+  : "ws://localhost:5555";
+
+const TEST_JSONRPC_METHOD = "test_method";
+const TEST_JSONRPC_REQUEST = { method: TEST_JSONRPC_METHOD, params: [] };
+const TEST_JSONRPC_RESULT = "it worked";
+
+const TEST_CHAINS = [];
+const TEST_METHODS = [TEST_JSONRPC_METHOD];
+
+const TEST_APP_METADATA = {
+  name: "Test App",
+  description: "Test App for WalletConnect",
+  url: "https://walletconnect.org/",
+  icons: ["https://walletconnect.org/walletconnect-logo.png"],
+};
+
+const TEST_WALLET_METADATA = {
+  name: "Test Wallet",
+  description: "Test Wallet for WalletConnect",
+  url: "https://walletconnect.org/",
+  icons: ["https://walletconnect.org/walletconnect-logo.png"],
+};
 
 describe("@walletconnect/ethereum-provider", () => {
   let testnetwork: TestNetwork;
@@ -28,18 +56,34 @@ describe("@walletconnect/ethereum-provider", () => {
     await testnetwork.close();
   });
 
-  it("needs tests", async () => {
-    // needs tests
-    const provider = new EthereumProvider({
-      rpc: {
-        custom: {
-          [CHAIN_ID]: RPC_URL,
+  it("Test enable", async () => {
+    try {
+      const clientB = await Client.init({
+        controller: true,
+        relayProvider: TEST_RELAY_URL,
+        metadata: TEST_WALLET_METADATA,
+      });
+      const signer = new SignerConnection({
+        chains: [CHAIN_ID.toString()],
+        client: clientB,
+      });
+      const provider = new EthereumProvider({
+        rpc: {
+          custom: {
+            [CHAIN_ID]: RPC_URL,
+          },
         },
-      },
-      chainId: CHAIN_ID,
-    });
-    // await provider.connect();
-    // console.log(provider.accounts);
-    expect(provider.chainId === CHAIN_ID, "chainId set");
+        chainId: CHAIN_ID,
+        methods: ["eth_requestAccounts"],
+        client: clientB,
+      });
+      await provider.connect();
+      const accounts = await provider.enable();
+      console.log(accounts);
+      // console.log(provider.accounts);
+      expect(accounts.length > 0, "Accounts is array and more then 0");
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
