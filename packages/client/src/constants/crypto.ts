950e1e5557122f22331f120d89074397e72f2419
Pedro Gomes <pedrogomes94@gmail.com>
2021-05-07 19:22:54 +0200

    unnecessary constants

--- packages/client/src/constants/crypto.ts
+++ packages/client/src/constants/crypto.ts
@@ -1,7 +1,3 @@
 export const CRYPTO_CONTEXT = "crypto";
 
-export const CRYPTO_PUBLIC_KEYS = "publickeys";
-
-export const CRYPTO_SHARED_KEYS = "sharedkeys";
-
 export const KEYCHAIN_CONTEXT = "keychain";
