d83c983fd4edc1c541c4384e8408eae46356a7c5
Sebastian Bolanos <sebasheston@protonmail.com>
2021-05-11 16:08:33 +0100

    Update waku.walletconnect.org.nix

--- ops/waku.walletconnect.org.nix
+++ ops/waku.walletconnect.org.nix
@@ -7,7 +7,7 @@ let
   waku = pkgs.dockerTools.pullImage {
         imageName = image;
         finalImageTag = tag;
-        imageDigest = "sha256:9b7f0ce6d1c59dd9ebc418b729a71b21f24ef2bb384b50977861b661250002f1";
+        imageDigest = "sha256:fe01e0c11fce793f2b80eba3e5c7a0d1976c9b30be790c33af8a50fe261714ff";
         sha256 = "000003zq2v6rrhizgb9nvhczl87lcfphq9601wcprdika2jz7qh8";
   };
 in {
@@ -15,6 +15,7 @@ in {
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 80 443 wakuP2P ];
+      allowedUDPPorts = [ wakuP2P ];
     };
   };
   services.fail2ban = {
@@ -38,11 +39,15 @@ in {
         "${volumePath}:/store"
       ];
       cmd = [
+        "--tpc-port=${wakuP2P}"
+        "--udp-port=${wakuP2P}"
         "--nodekey=1107ad8e44fe7dc924bb9d388d588832cdc4273efb2623e8609c8085d0d2154c"
         "--peerpersist=true"
+        "--keep-alive=true"
         "--rpc=false"
         "--relay=true"
         "--store=true"
+        "--persist-messages=true"
         "--filter=true"
         "--dbpath=/store"
         "--topics=6d9b0b4b9994e8a6afbd3dc3ed983cd51c755afb27cd1dc7825ef59c134a39f7"
