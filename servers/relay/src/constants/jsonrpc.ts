e7af984821166a359faffba867223b3e6d404ee3
Pedro Gomes <pedrogomes94@gmail.com>
2021-05-18 12:45:28 +0200

    refactor services to emit events

--- servers/relay/src/constants/jsonrpc.ts
+++ servers/relay/src/constants/jsonrpc.ts
@@ -3,3 +3,10 @@ import { THIRTY_SECONDS } from "./time";
 export const JSONRPC_RETRIAL_TIMEOUT = THIRTY_SECONDS * 1000;
 
 export const JSONRPC_RETRIAL_MAX = 3;
+
+export const JSONRPC_EVENTS = {
+  publish: "jsonrpc_publish",
+  subscribe: "jsonrpc_subscribe",
+  unsubscribe: "jsonrpc_unsubscribe",
+  subscription: "jsonrpc_subscription",
+};
