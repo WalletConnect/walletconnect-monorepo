1eb6ae7beeaa741850a5d81d45443415f5fe2e53
System administrator <sebasheston@pm.me>
2021-06-04 17:37:23 +0000

    Rever to old grafana subdomain

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+source setup
 
 monitoring=${MONITORING:-true} # this makes a bash string, not a boolean
 port=${UPSTREAM_PORT:-5000}
@@ -6,6 +7,7 @@ port=${UPSTREAM_PORT:-5000}
 run="docker stack deploy $PROJECT -c ops/docker-compose.yml -c ops/docker-compose.prod.yml "
 
 if [[ $monitoring != false ]]; then
+  sed "s|{{env \"DOMAIN\"}}|$RELAY_URL|g" ops/grafana/grafana.ini.tmpl > ops/grafana/grafana.ini
   run="${run} -c ops/docker-compose.monitor.yml"
 fi
 
