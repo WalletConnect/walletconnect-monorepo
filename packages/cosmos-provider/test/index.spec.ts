7107da19bacca14d65d023845b3846e6aedd6a92
Pedro Gomes <pedrogomes94@gmail.com>
2021-06-09 21:07:36 +0200

    cosmos unit testing

--- packages/cosmos-provider/test/index.spec.ts
+++ packages/cosmos-provider/test/index.spec.ts
@@ -1,19 +1,47 @@
 import "mocha";
 import { expect } from "chai";
-// import { CosmosWallet } from "cosmos-wallet";
+import { CosmosWallet } from "cosmos-wallet";
+import { fromHex, toHex } from "@cosmjs/encoding";
+import { AccountData, coins, makeSignDoc, makeAuthInfoBytes } from "@cosmjs/proto-signing";
 import { SIGNER_EVENTS } from "@walletconnect/signer-connection";
 import { Client, CLIENT_EVENTS } from "@walletconnect/client";
 import { SessionTypes } from "@walletconnect/types";
 
 import CosmosProvider from "./../src/index";
+import { formatJsonRpcError, formatJsonRpcResult, JsonRpcResponse } from "@json-rpc-tools/utils";
 
 const NAMESPACE = "cosmos";
 const CHAIN_ID = "cosmoshub-4";
 const RPC_URL = `https://rpc.cosmos.network/`;
 
-const wallet = {
-  address: "cosmos1t2uflqwqe0fsj0shcfkrvpukewcw40yjj6hdc0",
-} as any;
+export const TEST_COSMOS_INPUTS = {
+  direct: {
+    fee: coins(2000, "ucosm"),
+    pubkey: "AgSEjOuOr991QlHCORRmdE5ahVKeyBrmtgoYepCpQGOW",
+    gasLimit: 200000,
+    accountNumber: 1,
+    sequence: 1,
+    bodyBytes:
+      "0a90010a1c2f636f736d6f732e62616e6b2e763162657461312e4d736753656e6412700a2d636f736d6f7331706b707472653766646b6c366766727a6c65736a6a766878686c63337234676d6d6b38727336122d636f736d6f7331717970717870713971637273737a673270767871367273307a716733797963356c7a763778751a100a0575636f736d120731323334353637",
+    authInfoBytes:
+      "0a500a460a1f2f636f736d6f732e63727970746f2e736563703235366b312e5075624b657912230a21034f04181eeba35391b858633a765c4a0c189697b40d216354d50890d350c7029012040a020801180112130a0d0a0575636f736d12043230303010c09a0c",
+  },
+  amino: {
+    msgs: [],
+    fee: { amount: [], gas: "23" },
+    chain_id: "foochain",
+    memo: "hello, world",
+    account_number: "7",
+    sequence: "54",
+  },
+};
+
+export const TEST_COSMOS_KEYPAIR = {
+  publicKey: "0204848ceb8eafdf754251c2391466744e5a85529ec81ae6b60a187a90a9406396",
+  privateKey: "366cd8d38f760f970bdc70b18d19f40756b92beeebc84074ceea8e092d406666",
+};
+
+export const TEST_COSMOS_ADDRESS = "cosmos1sguafvgmel6f880ryvq8efh9522p8zvmrzlcrq";
 
 export const TEST_RELAY_URL = process.env.TEST_RELAY_URL
   ? process.env.TEST_RELAY_URL
@@ -40,8 +68,59 @@ const TEST_WALLET_METADATA = {
   icons: ["https://walletconnect.org/walletconnect-logo.png"],
 };
 
+export const TEST_COSMOS_DIRECT_SIGNATURE =
+  "LVtl91xbrxCTR643RZMw08uHV3tR5aL46iMiVnAFdWVoaQJN/+jpbs6GPyOOBgZW6nWldiB/WxGmMMoEHoCudQ==";
+
+export const TEST_COSMOS_AMINO_SIGNATURE =
+  "AnTrXtS2lr9CBwhTpRa8ZlKcVR9PeIXGaTpvodyJU05QvRKVjIkQfOZl5JhdkfxCY+a6rhwCOYVcbKQTJlMw4w==";
+
+function stringifySignDoc(signDoc: any) {
+  return {
+    ...signDoc,
+    bodyBytes: toHex(signDoc.bodyBytes),
+    authInfoBytes: toHex(signDoc.authInfoBytes),
+  };
+}
+
+function parseSignDoc(signDoc: any) {
+  return {
+    ...signDoc,
+    bodyBytes: fromHex(signDoc.bodyBytes),
+    authInfoBytes: fromHex(signDoc.authInfoBytes),
+  };
+}
+
+function generateDirectSignDoc() {
+  const { fee, pubkey, gasLimit, accountNumber, sequence, bodyBytes } = TEST_COSMOS_INPUTS.direct;
+  const authInfoBytes = makeAuthInfoBytes([pubkey as any], fee, gasLimit, sequence);
+  const signDoc = makeSignDoc(fromHex(bodyBytes), authInfoBytes, CHAIN_ID, accountNumber);
+  return stringifySignDoc(signDoc);
+}
+
+function stringifyAccount(account: AccountData) {
+  return { ...account, pubkey: toHex(account.pubkey) };
+}
+
+async function getAccounts(wallet: CosmosWallet) {
+  return (await wallet.getAccounts()).map(stringifyAccount);
+}
+
+async function signDirect(wallet: CosmosWallet, signerAddress: string, signDoc: any) {
+  const result = await wallet.signDirect(signerAddress, parseSignDoc(signDoc));
+  return {
+    signed: stringifySignDoc(result.signed),
+    signature: result.signature,
+  };
+}
+
+async function signAmino(wallet: CosmosWallet, signerAddress: string, signDoc: any) {
+  const result = await wallet.signAmino(signerAddress, signDoc);
+  return result;
+}
+
 describe("@walletconnect/cosmos-provider", () => {
-  it("Test connect", async () => {
+  it("Test connect and sign", async () => {
+    const wallet = await CosmosWallet.init(TEST_COSMOS_KEYPAIR.privateKey);
     const walletClient = await Client.init({
       controller: true,
       relayProvider: TEST_RELAY_URL,
@@ -64,11 +143,12 @@ describe("@walletconnect/cosmos-provider", () => {
     provider.signer.connection.on(SIGNER_EVENTS.uri, ({ uri }) => walletClient.pair({ uri }));
     // connect
     let accounts: string[] = [];
-
     await Promise.all([
       new Promise<void>((resolve, reject) => {
         walletClient.on(CLIENT_EVENTS.session.proposal, async (proposal: SessionTypes.Proposal) => {
-          const response = { state: { accounts: [`${wallet.address}@${NAMESPACE}:${CHAIN_ID}`] } };
+          const response = {
+            state: { accounts: [`${TEST_COSMOS_ADDRESS}@${NAMESPACE}:${CHAIN_ID}`] },
+          };
           await walletClient.approve({
             proposal,
             response,
@@ -82,9 +162,68 @@ describe("@walletconnect/cosmos-provider", () => {
         resolve();
       }),
     ]);
-    expect(
-      accounts[0].split("@")[0] === wallet.address,
-      "Returned account address is equal to test address",
-    ); // TODO Fails because of this, TypeError: Cannot read property 'split' of undefined
+    expect(accounts[0].split("@")[0]).to.eql(TEST_COSMOS_ADDRESS);
+
+    // auto-respond
+    walletClient.on(
+      CLIENT_EVENTS.session.request,
+      async (requestEvent: SessionTypes.RequestEvent) => {
+        let response: JsonRpcResponse;
+        try {
+          let result: any;
+          switch (requestEvent.request.method) {
+            case "cosmos_getAccounts":
+              result = await getAccounts(wallet);
+              break;
+            case "cosmos_signDirect":
+              result = await signDirect(
+                wallet,
+                requestEvent.request.params.signerAddress,
+                requestEvent.request.params.signDoc,
+              );
+              break;
+            case "cosmos_signAmino":
+              result = await signAmino(
+                wallet,
+                requestEvent.request.params.signerAddress,
+                requestEvent.request.params.signDoc,
+              );
+              break;
+            default:
+              throw new Error("Unsupported method");
+          }
+          response = formatJsonRpcResult(requestEvent.request.id, result);
+        } catch (e) {
+          response = formatJsonRpcError(requestEvent.request.id, e.message);
+        }
+        await walletClient.respond({
+          topic: requestEvent.topic,
+          response,
+        });
+      },
+    );
+
+    // cosmos_getAccounts
+    const accountsResult = await provider.request({ method: "cosmos_getAccounts" });
+
+    expect(!!accountsResult).to.be.true;
+    expect((accountsResult as any)[0].pubkey).to.eql(TEST_COSMOS_KEYPAIR.publicKey);
+
+    // cosmos_signDirect
+    const directResult = await provider.request({
+      method: "cosmos_signDirect",
+      params: { signerAddress: TEST_COSMOS_ADDRESS, signDoc: generateDirectSignDoc() },
+    });
+    expect(!!directResult).to.be.true;
+    expect((directResult as any).signature.signature).to.eql(TEST_COSMOS_DIRECT_SIGNATURE);
+
+    // cosmos_signDirect
+    const aminoResult = await provider.request({
+      method: "cosmos_signDirect",
+      params: { signerAddress: TEST_COSMOS_ADDRESS, signDoc: TEST_COSMOS_INPUTS.amino },
+    });
+
+    expect(!!aminoResult).to.be.true;
+    expect((aminoResult as any).signature.signature).to.eql(TEST_COSMOS_AMINO_SIGNATURE);
   });
 });
