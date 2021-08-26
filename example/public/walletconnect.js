b26e4e9dc40084b5ea4b88b603e7ca4f5d78b185
Pedro Gomes <pedrogomes94@gmail.com>
2021-08-26 13:26:51 +0200

    fix android and improve fetching logic (qrcode-modal)

--- example/public/walletconnect.js
+++ example/public/walletconnect.js
@@ -15,7 +15,6 @@ function onInit() {
   connector = new WalletConnect({
     bridge: DEFAULT_BRIDGE, // Required
     qrcodeModal: WalletConnectQRCodeModal,
-    qrcodeModalOptions: {},
   });
 
   // Check if connection is already established
