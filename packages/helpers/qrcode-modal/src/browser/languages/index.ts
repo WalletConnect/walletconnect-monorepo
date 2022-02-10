8adac91001760704429e976c881d74169be1d43d
Kuan Ting Lin <kusakawazeusu@gmail.com>
2022-02-10 21:37:57 +0800

    add language: zh_TW (traditional Chinese)

--- packages/helpers/qrcode-modal/src/browser/languages/index.ts
+++ packages/helpers/qrcode-modal/src/browser/languages/index.ts
@@ -8,7 +8,8 @@ import ko from "./ko";
 import pt from "./pt";
 import zh from "./zh";
 import fa from "./fa";
+import zh_tw from "./zh_tw";
 
-const languages: { [lang: string]: TextMap } = { de, en, es, fr, ko, pt, zh, fa };
+const languages: { [lang: string]: TextMap } = { de, en, es, fr, ko, pt, zh, fa, zh_tw };
 
 export default languages;
---
+++ packages/helpers/qrcode-modal/src/browser/languages/zh_tw.ts
@@ -0,0 +1,20 @@
+import { TextMap } from "../types";
+
+const zh_tw: TextMap = {
+  choose_preferred_wallet: "選擇你的錢包",
+  connect_mobile_wallet: "連結手機版錢包",
+  scan_qrcode_with_wallet: "使用支援 WalletConnect 的錢包掃描 QR Code",
+  connect: "連結",
+  qrcode: "QR Code",
+  mobile: "手機",
+  desktop: "桌面",
+  copy_to_clipboard: "複製內容",
+  copied_to_clipboard: "內容複製成功！",
+  connect_with: "透過以下方式連結錢包：",
+  loading: "載入中......",
+  something_went_wrong: "發生錯誤",
+  no_supported_wallets: "目前沒有支援的錢包",
+  no_wallets_found: "找不到錢包",
+};
+
+export default zh_tw;
