cbabf365d61508bed269c738eaa81c65d3842e95
Ben Kremer <contact@bkrem.dev>
2022-04-01 15:32:04 +0200

    chore(core): export all constants

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,14 @@
+// Re-exports
 export * as heartbeat from "@walletconnect/heartbeat";
 export * as logger from "@walletconnect/logger";
 
+// Export controllers with own namespace
 export * as subscriber from "./subscriber";
 export * as publisher from "./publisher";
 export * as messages from "./messages";
 export * as storage from "./storage";
 export * as relayer from "./relayer";
 export * as crypto from "./crypto";
+
+// Export all constants
+export * from "./constants";
