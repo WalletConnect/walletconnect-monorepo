5174a5cb7fea6413b8590a3d2f5702b5646d4933
Ben Kremer <contact@bkrem.dev>
2022-04-01 11:56:19 +0200

    chore(core): adds `messages` controller

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,5 +4,5 @@ export * as logger from "@walletconnect/logger";
 export * as crypto from "./crypto";
 export * as subscriber from "./subscriber";
 export * as publisher from "./publisher";
-
+export * as messages from "./messages";
 
