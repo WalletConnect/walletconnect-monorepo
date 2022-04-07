27478a3a3546a619692f26b8822ff7df2999983a
Ben Kremer <contact@bkrem.dev>
2022-04-07 15:22:16 +0200

    fix(crypto): use `formatStorageKeyName` for `storageKey` getter

--- packages/client/src/controllers/crypto.ts
+++ packages/client/src/controllers/crypto.ts
@@ -14,6 +14,7 @@ import {
   generateRandomBytes32,
   objToMap,
   mapToObj,
+  formatStorageKeyName,
 } from "@walletconnect/utils";
 
 import { CRYPTO_CONTEXT, KEYCHAIN_CONTEXT, KEYCHAIN_STORAGE_VERSION } from "../constants";
@@ -35,7 +36,7 @@ export class KeyChain implements IKeyChain {
   }
 
   get storageKey(): string {
-    return this.client.storagePrefix + this.version + "//" + "crypto:keychain";
+    return this.client.storagePrefix + this.version + "//" + formatStorageKeyName(this.context);
   }
 
   public async init(): Promise<void> {
