7ff61bab4233fabcb9558b7c89f9dc259c6bbe82
Ben Kremer <ben@walletconnect.com>
2022-04-28 16:17:07 +0700

    test(core): sets up initial tests for core.ts

--- packages/core/test/core.spec.ts
+++ packages/core/test/core.spec.ts
@@ -1,9 +1,21 @@
 import "mocha";
+import sinon from "sinon";
 
-// import Core from "../src";
+import Core, { CORE_CONTEXT, CORE_PROTOCOL, CORE_VERSION } from "../src";
+import { expect, TEST_CORE_OPTIONS } from "./shared";
 
 describe("Core", () => {
-  it("needs tests", async () => {
-    // needs tests
+  it("provides the expected `storagePrefix` format", () => {
+    const core = new Core(TEST_CORE_OPTIONS);
+    expect(core.storagePrefix).to.equal(`${CORE_PROTOCOL}@${CORE_VERSION}:${CORE_CONTEXT}:`);
+  });
+  it("does not duplicate initilization if `Core.start()` is called repeatedly", async () => {
+    const core = new Core(TEST_CORE_OPTIONS);
+    const initSpy = sinon.spy();
+    // Spy on `crypto.init` as a proxy to the private `Core.initialize`.
+    core.crypto.init = initSpy;
+    await core.start();
+    await core.start();
+    expect(initSpy.callCount).to.equal(1);
   });
 });
