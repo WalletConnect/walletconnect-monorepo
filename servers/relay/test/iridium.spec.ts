e02404f29c0cfd8f8982c56b8983da147b93d8db
Sebastian Bolanos <sebasheston@protonmail.com>
2022-04-28 16:30:44 +0100

    Formatter and add byte payload length

--- servers/relay/test/iridium.spec.ts
+++ servers/relay/test/iridium.spec.ts
@@ -7,8 +7,18 @@ import { IridiumEncoder } from "../src/encoder";
 const encoder = new IridiumEncoder();
 describe("IRIDIUM", () => {
   it("Decode Test", async () => {
+    let payload = "1";
+    const encoded = await encoder.encode(payload);
+    const {
+      message,
+      opts: { prompt },
+    } = await encoder.decode(encoded);
+    expect(message).to.equal(payload);
+    expect(prompt).to.be.false;
+  });
+  it("Decode Test", async () => {
     let payload = "4b7e34e9c9ae95fc421a138f205de4d670e68ad93accf5a7fefa0af1a761c2b1".repeat(2);
-    const encoded  = await encoder.encode(payload)
+    const encoded = await encoder.encode(payload);
     const {
       message,
       opts: { prompt },
@@ -19,7 +29,7 @@ describe("IRIDIUM", () => {
   it("Decode Test", async () => {
     const encoder = new IridiumEncoder();
     let payload = "4b".repeat(4);
-    const encoded  = await encoder.encode(payload)
+    const encoded = await encoder.encode(payload);
     const {
       message,
       opts: { prompt },
