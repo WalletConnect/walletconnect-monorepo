adf32c9fa23d5a9795266dc42c5e09e873afa8c4
Ben Kremer <ben@walletconnect.com>
2022-04-29 16:57:44 +0700

    test(core): sets up test suite for `relayer.ts`

---
+++ packages/core/test/relayer.spec.ts
@@ -0,0 +1,139 @@
+import "mocha";
+import { getDefaultLoggerOptions } from "@walletconnect/logger";
+import pino from "pino";
+
+import {
+  Core,
+  CORE_DEFAULT,
+  Relayer,
+  RELAYER_EVENTS,
+  RELAYER_PROVIDER_EVENTS,
+  RELAYER_SUBSCRIBER_SUFFIX,
+} from "../src";
+import { expect, TEST_CORE_OPTIONS } from "./shared";
+import { ICore, IRelayer } from "@walletconnect/types";
+import Sinon from "sinon";
+import { JsonRpcRequest } from "@walletconnect/jsonrpc-utils";
+
+// TODO: Test persistence behavior
+describe("Relayer", () => {
+  const logger = pino(getDefaultLoggerOptions({ level: CORE_DEFAULT.logger }));
+
+  let core: ICore;
+  let relayer: IRelayer;
+
+  beforeEach(() => {
+    core = new Core(TEST_CORE_OPTIONS);
+    relayer = new Relayer({ core, logger });
+  });
+
+  it("registers event listeners when constructed", () => {
+    const emitSpy = Sinon.spy();
+    relayer.events.emit = emitSpy;
+    relayer.provider.events.emit(RELAYER_PROVIDER_EVENTS.connect);
+    expect(emitSpy.calledOnceWith(RELAYER_EVENTS.connect)).to.be.true;
+  });
+
+  describe("init", () => {
+    let initSpy: Sinon.SinonSpy;
+    beforeEach(() => {
+      // Mock `provider.connect` to avoid dependency on relay server here.
+      relayer.provider.connect = () => Promise.resolve();
+      initSpy = Sinon.spy();
+    });
+
+    it("initializes a MessageTracker", async () => {
+      relayer.messages.init = initSpy;
+      await relayer.init();
+      expect(initSpy.calledOnce).to.be.true;
+    });
+    it("initializes a Subscriber", async () => {
+      relayer.subscriber.init = initSpy;
+      await relayer.init();
+      expect(initSpy.calledOnce).to.be.true;
+    });
+    it("initializes a Publisher", async () => {
+      relayer.subscriber.init = initSpy;
+      await relayer.init();
+      expect(initSpy.calledOnce).to.be.true;
+    });
+    it("calls provider.connect", async () => {
+      relayer.provider.connect = initSpy;
+      await relayer.init();
+      expect(initSpy.calledOnce).to.be.true;
+    });
+  });
+
+  describe("publish", () => {
+    const topic = "abc123";
+    const message = "publish me";
+    it("calls `publisher.publish` with provided args", async () => {
+      const spy = Sinon.spy();
+      relayer.publisher.publish = spy;
+      await relayer.publish(topic, message);
+      expect(spy.calledOnceWith(topic, message)).to.be.true;
+    });
+    it("records a message with provided args", async () => {
+      const spy = Sinon.spy();
+      relayer.publisher.publish = () => Promise.resolve();
+      relayer.messages.set = spy;
+      await relayer.publish(topic, message);
+      expect(spy.calledOnceWith(topic, message)).to.be.true;
+    });
+  });
+
+  describe("subscribe", () => {
+    it("returns the id provided by calling `subscriber.subscribe` with the passed topic", async () => {
+      const spy = Sinon.spy(() => "mock-id");
+      relayer.subscriber.subscribe = spy;
+      const id = await relayer.subscribe("abc123");
+      expect(spy.calledOnceWith("abc123")).to.be.true;
+      expect(id).to.eq("mock-id");
+    });
+  });
+
+  describe("unsubscribe", () => {
+    it("calls `subscriber.unsubscribe` with the passed topic", async () => {
+      const spy = Sinon.spy();
+      relayer.subscriber.unsubscribe = spy;
+      await relayer.unsubscribe("abc123");
+      expect(spy.calledOnceWith("abc123")).to.be.true;
+    });
+  });
+
+  describe("onProviderPayload", () => {
+    const validPayload: JsonRpcRequest = {
+      id: 123,
+      jsonrpc: "2.0",
+      method: "mock" + RELAYER_SUBSCRIBER_SUFFIX,
+      params: { id: "abc123", data: { topic: "ababab", message: "deadbeef" } },
+    };
+
+    it("does nothing if payload is not a valid JsonRpcRequest", () => {
+      const spy = Sinon.spy();
+      relayer.events.emit = spy;
+      relayer.provider.events.emit(RELAYER_PROVIDER_EVENTS.payload, {});
+      expect(spy.notCalled).to.be.true;
+    });
+    it(`does nothing if payload.method does not have the ${RELAYER_SUBSCRIBER_SUFFIX} suffix`, () => {
+      const spy = Sinon.spy();
+      relayer.events.emit = spy;
+      relayer.provider.events.emit(RELAYER_PROVIDER_EVENTS.payload, {
+        ...validPayload,
+        method: "mock",
+      });
+      expect(spy.notCalled).to.be.true;
+    });
+    it("emits an event based on `payload.params.id`", () => {
+      const spy = Sinon.spy();
+      relayer.events.emit = spy;
+      relayer.provider.events.emit(RELAYER_PROVIDER_EVENTS.payload, validPayload);
+      expect(
+        spy.calledOnceWith(validPayload.params.id, {
+          topic: validPayload.params.data.topic,
+          message: validPayload.params.data.message,
+        }),
+      ).to.be.true;
+    });
+  });
+});
