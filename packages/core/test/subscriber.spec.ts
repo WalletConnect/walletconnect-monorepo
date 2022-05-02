6ff84a06f823c00d73291aa7d9644cdbd8a17ed2
Ben Kremer <ben@walletconnect.com>
2022-05-02 16:57:06 +0700

    test(core): sets up test suite for `subscriber.ts`

---
+++ packages/core/test/subscriber.spec.ts
@@ -0,0 +1,142 @@
+import "mocha";
+import pino from "pino";
+import Sinon from "sinon";
+import { getDefaultLoggerOptions } from "@walletconnect/logger";
+import { IRelayer, ISubscriber } from "@walletconnect/types";
+import { generateRandomBytes32, getRelayProtocolName } from "@walletconnect/utils";
+
+import {
+  Core,
+  CORE_DEFAULT,
+  CORE_STORAGE_PREFIX,
+  MESSAGES_STORAGE_VERSION,
+  Relayer,
+  RELAYER_PROVIDER_EVENTS,
+  Subscriber,
+  SUBSCRIBER_CONTEXT,
+} from "../src";
+import { expect, TEST_CORE_OPTIONS } from "./shared";
+
+describe("Subscriber", () => {
+  const logger = pino(getDefaultLoggerOptions({ level: CORE_DEFAULT.logger }));
+
+  let relayer: IRelayer;
+  let subscriber: ISubscriber;
+
+  beforeEach(async () => {
+    const core = new Core(TEST_CORE_OPTIONS);
+    relayer = new Relayer({ core, logger });
+    subscriber = new Subscriber(relayer, logger);
+    subscriber.relayer.provider.request = () => Promise.resolve({} as any);
+    await subscriber.init();
+  });
+
+  it("provides the expected `storageKey` format", () => {
+    const subscriber = new Subscriber(relayer, logger);
+    expect(subscriber.storageKey).to.equal(
+      CORE_STORAGE_PREFIX + MESSAGES_STORAGE_VERSION + "//" + SUBSCRIBER_CONTEXT,
+    );
+  });
+
+  describe("init", () => {
+    it("registers event listeners", async () => {
+      const topic = generateRandomBytes32();
+      const emitSpy = Sinon.spy();
+      subscriber.events.emit = emitSpy;
+      // subscribe to a topic
+      await subscriber.subscribe(topic);
+      expect(subscriber.subscriptions.size).to.equal(1);
+      expect(subscriber.topics.length).to.equal(1);
+      // relayer.provider emits a `disconnect` event -> should clear both subscriptions and topics.
+      relayer.provider.events.emit(RELAYER_PROVIDER_EVENTS.disconnect);
+      expect(subscriber.subscriptions.size).to.equal(0);
+      expect(subscriber.topics.length).to.equal(0);
+    });
+  });
+
+  describe("subscribe", () => {
+    let topic: string;
+    let requestSpy: Sinon.SinonSpy;
+
+    beforeEach(() => {
+      requestSpy = Sinon.spy(() => "test-id");
+      topic = generateRandomBytes32();
+      subscriber.relayer.provider.request = requestSpy;
+    });
+
+    it("throws if Subscriber was not initialized", async () => {
+      const subscriber = new Subscriber(relayer, logger);
+      await expect(subscriber.subscribe(topic)).to.eventually.be.rejectedWith(
+        "subscription was not initialized",
+      );
+    });
+    it("calls `provider.request` with the expected request shape", async () => {
+      await subscriber.subscribe(topic);
+      expect(
+        requestSpy.calledOnceWith(
+          Sinon.match({
+            method: "waku_subscribe",
+            params: {
+              topic,
+            },
+          }),
+        ),
+      ).to.be.true;
+    });
+    it("returns the subscription id", async () => {
+      const id = await subscriber.subscribe(topic);
+      expect(id).to.equal("test-id");
+    });
+  });
+
+  describe("unsubscribe", () => {
+    let topic: string;
+    let requestSpy: Sinon.SinonSpy;
+    let messageDeleteSpy: Sinon.SinonSpy;
+
+    beforeEach(() => {
+      requestSpy = Sinon.spy();
+      messageDeleteSpy = Sinon.spy();
+      topic = generateRandomBytes32();
+      subscriber.relayer.provider.request = requestSpy;
+      subscriber.relayer.messages.del = messageDeleteSpy;
+    });
+    it("throws if Subscriber was not initialized", async () => {
+      const subscriber = new Subscriber(relayer, logger);
+      await expect(subscriber.unsubscribe(topic)).to.eventually.be.rejectedWith(
+        "subscription was not initialized",
+      );
+    });
+    it("unsubscribes by individual id if `opts.id` is provided", async () => {
+      const id = "test-id";
+      await subscriber.unsubscribe(topic, { id, relay: getRelayProtocolName() });
+      expect(messageDeleteSpy.calledOnceWith(topic)).to.be.true;
+      expect(
+        requestSpy.calledOnceWith(
+          Sinon.match({
+            method: "waku_unsubscribe",
+            params: {
+              topic,
+            },
+          }),
+        ),
+      ).to.be.true;
+    });
+    it("unsubscribes by topic by default", async () => {
+      await subscriber.subscribe(topic);
+      expect(subscriber.topics.length).to.equal(1);
+      await subscriber.unsubscribe(topic);
+      expect(subscriber.topics.length).to.equal(0);
+      expect(
+        requestSpy.getCall(1).calledWith(
+          Sinon.match({
+            method: "waku_unsubscribe",
+            params: {
+              topic,
+            },
+          }),
+        ),
+      ).to.be.true;
+    });
+  });
+});
