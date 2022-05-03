b5c6449ef671cc4f38bbf7e81031104cf70537ce
Ilja <idaderko@gmail.com>
2022-05-03 19:02:42 +0300

    cleanup

--- packages/client/test/validation.spec.ts
+++ packages/client/test/validation.spec.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import "mocha";
 import {
   expect,
@@ -22,24 +23,25 @@ describe("Client Validation", () => {
 
   describe("connect", () => {
     it("throws when no params are passed", async () => {
-      // @ts-expect-error
       await expect(client.connect()).to.eventually.be.rejectedWith(
         "Missing or invalid connect params",
       );
     });
+
     it("throws when non existant pairingTopic is provided", async () => {
       await expect(
         client.connect({ ...TEST_CONNECT_PARAMS, pairingTopic: "none" }),
       ).to.eventually.be.rejectedWith("No matching pairing with topic: none");
     });
+
     it("throws when empty namespaces are provided", async () => {
       await expect(
         client.connect({ ...TEST_CONNECT_PARAMS, pairingTopic, namespaces: [] }),
       ).to.eventually.be.rejectedWith("Missing or invalid namespaces");
     });
+
     it("throws when invalid namespaces are provided", async () => {
       await expect(
-        // @ts-expect-error
         client.connect({ ...TEST_CONNECT_PARAMS, pairingTopic, namespaces: {} }),
       ).to.eventually.be.rejectedWith("Missing or invalid namespaces");
     });
@@ -47,16 +49,16 @@ describe("Client Validation", () => {
 
   describe("pair", () => {
     it("throws when undefined params are passed", async () => {
-      // @ts-expect-error
       await expect(client.pair()).to.eventually.be.rejectedWith("Missing or invalid uri");
     });
+
     it("throws when empty uri is provided", async () => {
       await expect(client.pair({ uri: "" })).to.eventually.be.rejectedWith(
         "Missing or invalid uri",
       );
     });
+
     it("throws when invalid uri is provided", async () => {
-      // @ts-expect-error
       await expect(client.pair({ uri: 123 })).to.eventually.be.rejectedWith(
         "Missing or invalid uri",
       );
@@ -65,12 +67,11 @@ describe("Client Validation", () => {
 
   describe("approve", () => {
     it("throws when undefined params are passed", async () => {
-      // @ts-expect-error
       await expect(client.approve()).to.eventually.be.rejectedWith("Missing or invalid uri");
     });
+
     it("throws when invalid id is provided", async () => {
       await expect(
-        // @ts-expect-error
         client.approve({ ...TEST_APPROVE_PARAMS, id: "123" }),
       ).to.eventually.be.rejectedWith("balegdeh");
     });
