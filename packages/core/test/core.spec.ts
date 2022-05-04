2257acad6d3bbff02265c2b2245133acce6b8905
Ben Kremer <ben@walletconnect.com>
2022-05-04 16:45:31 +0700

    test(core): avoids reliance on relay-server for `core.spec.ts`

--- packages/core/test/core.spec.ts
+++ packages/core/test/core.spec.ts
@@ -7,11 +7,17 @@ import { expect, TEST_CORE_OPTIONS } from "./shared";
 describe("Core", () => {
   it("does not duplicate initilization if `Core.start()` is called repeatedly", async () => {
     const core = new Core(TEST_CORE_OPTIONS);
-    const initSpy = sinon.spy();
-    // Spy on `crypto.init` as a proxy to the private `Core.initialize`.
-    core.crypto.init = initSpy;
+    const cryptoInitSpy = sinon.spy();
+    const relayerInitSpy = sinon.spy();
+    const heartbeatInitSpy = sinon.spy();
+    // Spy on subcontroller `init` as a proxy to the private `Core.initialize`.
+    core.crypto.init = cryptoInitSpy;
+    core.relayer.init = relayerInitSpy;
+    core.heartbeat.init = heartbeatInitSpy;
     await core.start();
     await core.start();
-    expect(initSpy.callCount).to.equal(1);
+    expect(cryptoInitSpy.callCount).to.equal(1);
+    expect(relayerInitSpy.callCount).to.equal(1);
+    expect(heartbeatInitSpy.callCount).to.equal(1);
   });
 });
