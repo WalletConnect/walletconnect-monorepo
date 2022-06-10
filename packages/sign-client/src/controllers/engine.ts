fca46b0680b12c6cfd95779d28bc2e574fdc37b4
Pedro Gomes <pedrogomes94@gmail.com>
2022-06-10 08:52:08 +0200

    fix falsy result values for respond

--- packages/sign-client/src/controllers/engine.ts
+++ packages/sign-client/src/controllers/engine.ts
@@ -246,7 +246,7 @@ export class Engine extends IEngine {
     const { done, resolve, reject } = createDelayedPromise<T>();
     this.events.once<"session_request">(engineEvent("session_request", id), ({ error, result }) => {
       if (error) reject(error);
-      else if (result) resolve(result);
+      else if (typeof result === "undefined") resolve(result);
     });
     return await done();
   };
