c4bca9cf686ebcaad56eea03220e3bfcdc421bdd
Gancho Radkov <ganchoradkov93@gmail.com>
2022-06-22 16:36:42 +0300

    feat(sign-client/test): adds validation test for some of the events

---
+++ packages/sign-client/test/events.spec.ts
@@ -0,0 +1,227 @@
+import { getSdkError, parseUri } from "@walletconnect/utils";
+import "mocha";
+import SignClient from "../src";
+import {
+  expect,
+  initTwoClients,
+  testConnectMethod,
+  TEST_SIGN_CLIENT_OPTIONS,
+  deleteClients,
+  TEST_NAMESPACES,
+  TEST_REQUIRED_NAMESPACES,
+  TEST_EMIT_PARAMS
+} from "./shared";
+import { EngineTypes, PairingTypes, SessionTypes } from "@walletconnect/types";
+
+describe("Sign Client Events Validation", () => {
+  it("init", async () => {
+    const client = await SignClient.init(TEST_SIGN_CLIENT_OPTIONS);
+    expect(client).to.be.exist;
+  });
+
+  describe("Events", () => {
+    it("session_proposal", async () => {
+
+      const clients = await initTwoClients();
+      const { A, B } = clients;
+
+      const connectParams: EngineTypes.ConnectParams = {
+        requiredNamespaces: TEST_REQUIRED_NAMESPACES,
+        relays: undefined,
+        pairingTopic: undefined,
+      };
+
+      const approveParams: Omit<EngineTypes.ApproveParams, "id"> = {
+        namespaces: TEST_NAMESPACES,
+      };
+
+      const { uri, approval } = await A.connect(connectParams);
+
+      let pairingA: PairingTypes.Struct | undefined;
+      let pairingB: PairingTypes.Struct | undefined;
+
+      if (!uri) throw new Error("uri is missing");
+
+      const uriParams = parseUri(uri);
+
+      pairingA = A.pairing.get(uriParams.topic);
+      expect(pairingA.topic).to.eql(uriParams.topic);
+      expect(pairingA.relay).to.eql(uriParams.relay);
+
+      if (!pairingA) throw new Error("expect pairing A to be defined");
+
+      let sessionA: SessionTypes.Struct;
+      let sessionB: SessionTypes.Struct;
+
+      await Promise.all([
+        new Promise<void>((resolve, reject) => {
+          B.once("session_proposal", async proposal => {
+            try {
+              expect(proposal.params.requiredNamespaces).to.eql(connectParams.requiredNamespaces);
+              const { acknowledged } = await B.approve({
+                id: proposal.id,
+                ...approveParams,
+              });
+              if (!sessionB) {
+                sessionB = await acknowledged();
+              }
+              resolve();
+            } catch (e) {
+              reject(e);
+            }
+          });
+        }),
+        new Promise<void>(async (resolve, reject) => {
+          try {
+            if (uri) {
+              pairingB = await B.pair({ uri });
+              if (!pairingA) throw new Error("pairingA is missing");
+              expect(pairingB.topic).to.eql(pairingA.topic);
+              expect(pairingB.relay).to.eql(pairingA.relay);
+              resolve();
+            } else {
+              reject(new Error("missing uri"));
+            }
+          } catch (error) {
+            reject(error);
+          }
+        })
+        ,
+        new Promise<void>(async (resolve, reject) => {
+          try {
+            if (!sessionA) {
+              sessionA = await approval();
+            }
+            resolve();
+          } catch (error) {
+            reject(error);
+          }
+        })
+      ]);
+
+      deleteClients(clients)
+    });
+
+    it("session_update", async () => {
+      const clients = await initTwoClients();
+      const {
+        sessionA,
+      } = await testConnectMethod(clients);
+
+      await new Promise<void>(async (resolve, reject) => {
+        try {
+          
+          const namespacesBefore = sessionA.namespaces;
+          const namespacesAfter = {
+            ...namespacesBefore,
+            eip9001: {
+              accounts: ["eip9001:1:0x000000000000000000000000000000000000dead"],
+              methods: ["eth_sendTransaction"],
+              events: ["accountsChanged"],
+            },
+          };
+
+          clients.B.once("session_update", () => {
+            expect(clients.A.session.get(sessionA.topic).namespaces).to.eql(namespacesAfter);
+            resolve();
+          });
+          
+          const { acknowledged } = await clients.A.update({ topic: sessionA.topic , namespaces: namespacesAfter })
+          await acknowledged()
+        } catch (e) {
+          reject(e);
+        }
+      })
+
+      deleteClients(clients);
+    });
+
+    it("session_ping", async () => {
+      const clients = await initTwoClients();
+      const {
+        sessionA,
+      } = await testConnectMethod(clients);
+
+      await new Promise<void>(async (resolve, reject) => {
+        try {
+          clients.B.once("session_ping", event => {
+            expect(sessionA.topic).to.eql(event.topic);
+            resolve();
+          });
+          
+          await clients.A.ping({ topic: sessionA.topic }) 
+        } catch (e) {
+          reject(e);
+        }
+      })
+
+      deleteClients(clients);
+    });
+
+    it("session_event", async () => {
+        const clients = await initTwoClients();
+
+        const connectParams: EngineTypes.ConnectParams = {
+          requiredNamespaces: TEST_REQUIRED_NAMESPACES,
+          relays: undefined,
+          pairingTopic: undefined,
+        };
+
+        const {
+          sessionA,
+        } = await testConnectMethod(clients, connectParams);
+
+        const eventPayload: EngineTypes.EmitParams = {
+          topic: sessionA.topic,
+          ...TEST_EMIT_PARAMS
+        }
+
+        await new Promise<void>(async (resolve, reject) => {
+          try {
+            clients.B.on("session_event", event => {
+              expect(TEST_EMIT_PARAMS).to.eql(event.params);
+              expect(eventPayload.topic).to.eql(event.topic);
+              resolve();
+            });
+            
+            clients.A.emit(eventPayload)
+          } catch (e) {
+            reject(e);
+          }
+        })
+        deleteClients(clients);
+      });
+    it("session_delete", async () => {
+      const clients = await initTwoClients();
+
+      const connectParams: EngineTypes.ConnectParams = {
+        requiredNamespaces: TEST_REQUIRED_NAMESPACES,
+        relays: undefined,
+        pairingTopic: undefined,
+      };
+
+      const {
+        sessionA,
+      } = await testConnectMethod(clients, connectParams);
+
+      const eventPayload: EngineTypes.EmitParams = {
+        topic: sessionA.topic,
+        ...TEST_EMIT_PARAMS
+      }
+
+      await new Promise<void>(async (resolve, reject) => {
+        try {
+          clients.B.on("session_delete", event => {
+            expect(eventPayload.topic).to.eql(event.topic);
+            resolve();
+          });
+          
+          clients.A.disconnect({ topic: sessionA.topic, reason: getSdkError('USER_DISCONNECTED')});
+        } catch (e) {
+          reject(e);
+        }
+      })
+      deleteClients(clients);
+    });
+  });
+});
