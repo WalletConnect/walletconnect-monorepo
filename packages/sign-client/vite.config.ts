c7f966a176f42c1b96d930a47c6db893a3f2b146
Ilja <idaderko@gmail.com>
2022-06-24 19:06:45 +0300

    stash

--- packages/sign-client/vite.config.ts
+++ packages/sign-client/vite.config.ts
@@ -1,18 +1,15 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import analyze from "rollup-plugin-analyzer";
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "@walletconnect/sign-client",
-      formats: ["cjs", "es", "umd"],
+      formats: ["es", "umd"],
       fileName: "index",
     },
-    rollupOptions: {
-      plugins: [analyze()],
-    },
+
     outDir: resolve(__dirname, "dist"),
   },
 });
