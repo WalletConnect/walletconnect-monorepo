4dcd8dcf387e6a16c51624396d96967e00ad0121
Ilja <idaderko@gmail.com>
2022-06-25 09:42:59 +0300

    create global config

--- packages/sign-client/vite.config.ts
+++ packages/sign-client/vite.config.ts
@@ -1,21 +1,9 @@
 import { resolve } from "path";
-import { defineConfig } from "vite";
 import { name } from "./package.json";
+import createConfig from "../../vite.config";
 
-export default defineConfig({
-  build: {
-    lib: {
-      name,
-      entry: resolve(__dirname, "src/index.ts"),
-      formats: ["es", "umd", "cjs"],
-      fileName: "index",
-    },
-    rollupOptions: {
-      output: {
-        exports: "named",
-      },
-    },
-
-    outDir: resolve(__dirname, "dist"),
-  },
+export default createConfig({
+  name,
+  entry: resolve(__dirname, "src/index.ts"),
+  outDir: resolve(__dirname, "dist"),
 });
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+interface IConfigOptions {
+  name: string;
+  entry: string;
+  outDir: string;
+}
+
+export default function createConfig({ name, entry, outDir }: IConfigOptions) {
+  return defineConfig({
+    build: {
+      lib: {
+        name,
+        entry,
+        formats: ["es", "umd", "cjs"],
+        fileName: "index",
+      },
+
+      rollupOptions: {
+        output: {
+          exports: "named",
+        },
+      },
+
+      outDir,
+    },
+  });
+}
