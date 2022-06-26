06e9da1afe03d274e94c059176e6dbaf41c40cc3
Ilja <idaderko@gmail.com>
2022-06-26 17:01:09 +0300

    stash

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import polyfillNode from "rollup-plugin-polyfill-node";
 
 interface IConfigOptions {
   name: string;
@@ -15,14 +16,21 @@ export default function createConfig({ name, entry, outDir }: IConfigOptions) {
         formats: ["es", "umd", "cjs"],
         fileName: "index",
       },
-
       rollupOptions: {
         output: {
           exports: "named",
         },
+        plugins: [polyfillNode({ include: ["events"] })],
       },
-
       outDir,
+    },
+    optimizeDeps: {
+      exclude: ["events"],
+      esbuildOptions: {
+        define: {
+          global: "globalThis",
+        },
+      },
     },
   });
 }
