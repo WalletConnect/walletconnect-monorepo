d9cee62c26f5923d4c5501e9b2e10e574e0dfc52
Ilja <idaderko@gmail.com>
2022-06-26 20:07:23 +0300

    use rollup

---
+++ packages/utils/rollup.config.js
@@ -0,0 +1,12 @@
+import typescript from "@rollup/plugin-typescript";
+import { name } from "./package.json";
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    { file: "./dist/index.cjs.js", format: "cjs", name },
+    { file: "./dist/index.es.js", format: "es", name },
+    { file: "./dist/index.umd.js", format: "umd", name },
+  ],
+  plugins: [typescript()],
+};
--- packages/utils/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { resolve } from "path";
-import { name } from "./package.json";
-import createConfig from "../../vite.config";
-
-export default createConfig({
-  name,
-  entry: resolve(__dirname, "src/index.ts"),
-  outDir: resolve(__dirname, "dist"),
-});
