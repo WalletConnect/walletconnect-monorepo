56134f93eebbeca987b92b5a218da6f68863a921
Ilja <idaderko@gmail.com>
2022-06-28 11:22:29 +0300

    save progress

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import nodePolyfills from "rollup-plugin-polyfill-node";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
-import { dependencies } from "./package.json";
 
 const coreConfig = {
   input: "./src/index.ts",
@@ -17,19 +16,19 @@ const coreConfig = {
   ],
 };
 
-export default function createConfig(name, packageDependencies) {
+export default function createConfig(packageName, packageDependencies) {
   return [
     {
       ...coreConfig,
       plugins: [nodeResolve({ preferBuiltins: false }), commonjs(), json(), ...coreConfig.plugins],
-      output: { file: "./dist/index.umd.js", format: "umd", exports: "named", name },
+      output: { file: "./dist/index.umd.js", format: "umd", exports: "named", name: packageName },
     },
     {
       ...coreConfig,
-      external: [...Object.keys(dependencies), ...packageDependencies],
+      external: packageDependencies,
       output: [
-        { file: "./dist/index.cjs.js", format: "cjs", exports: "named", name },
-        { file: "./dist/index.es.js", format: "es", exports: "named", name },
+        { file: "./dist/index.cjs.js", format: "cjs", exports: "named", name: packageName },
+        { file: "./dist/index.es.js", format: "es", exports: "named", name: packageName },
       ],
     },
   ];
