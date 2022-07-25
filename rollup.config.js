379537bfc993eb620a440db819cfc462032c4773
Ilja <idaderko@gmail.com>
2022-07-25 19:40:32 +0300

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -3,33 +3,28 @@ import nodePolyfills from "rollup-plugin-polyfill-node";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
-const coreConfig = {
-  input: "./src/index.ts",
-  plugins: [
-    nodePolyfills(),
-    esbuild({
-      minify: true,
-      tsconfig: "./tsconfig.json",
-      loaders: {
-        ".json": "json",
-      },
-    }),
-  ],
-};
+const input = "./src/index.ts";
+const plugins = [
+  nodePolyfills(),
+  esbuild({
+    minify: true,
+    tsconfig: "./tsconfig.json",
+    loaders: {
+      ".json": "json",
+    },
+  }),
+];
 
 export default function createConfig(packageName, packageDependencies) {
   return [
     {
-      ...coreConfig,
-      plugins: [
-        nodeResolve({ preferBuiltins: false, browser: true }),
-        commonjs(),
-        ...coreConfig.plugins,
-      ],
+      input,
+      plugins: [nodeResolve({ preferBuiltins: false, browser: true }), commonjs(), ...plugins],
       output: { file: "./dist/index.umd.js", format: "umd", exports: "named", name: packageName },
     },
     {
-      ...coreConfig,
+      input,
+      plugins,
       external: packageDependencies,
       output: [
         { file: "./dist/index.cjs.js", format: "cjs", exports: "named", name: packageName },
