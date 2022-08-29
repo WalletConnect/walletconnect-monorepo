81703ba888d090b277e935e9b2f30786890ba1bb
Gancho Radkov <ganchoradkov93@gmail.com>
2022-08-29 14:31:55 +0300

    refactor: uses standardized storage prefix

--- providers/universal-provider/src/constants/values.ts
+++ providers/universal-provider/src/constants/values.ts
@@ -2,4 +2,8 @@ export const LOGGER = "error";
 
 export const RELAY_URL = "wss://relay.walletconnect.com";
 
-export const STORAGE = "@universal_provider";
+export const PROTOCOL = "wc";
+export const WC_VERSION = 2;
+export const CONTEXT = "universal_provider";
+
+export const STORAGE = `${PROTOCOL}@${WC_VERSION}:${CONTEXT}:`;
