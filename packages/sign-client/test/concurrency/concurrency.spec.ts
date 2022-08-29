b970f3fc8f3f6dcce90c87035fbbf5fe99c56e1c
Derek <alexanderderekrein@gmail.com>
2022-08-29 16:51:58 -0400

    fix: not filtering out unsuccessful values

--- packages/sign-client/test/concurrency/concurrency.spec.ts
+++ packages/sign-client/test/concurrency/concurrency.spec.ts
@@ -123,8 +123,8 @@ describe("Sign Client Concurrency", () => {
               async (resolve) => {
                 const timeout = setTimeout(() => {
                   log(`Client ${i} hung up`);
-                  resolve({ handshakeLatencyMs: 0, pairingLatencyMs: 0 });
-                }, 90_000);
+                  resolve({ handshakeLatencyMs: -1, pairingLatencyMs: -1 });
+                }, 120_000);
 
                 const now = new Date().getTime();
                 const clients: Clients = await initTwoClients({ relayUrl });
