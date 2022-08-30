fb840b744d6abf75e4ec6ac05ed43189282dc895
Derek <alexanderderekrein@gmail.com>
2022-08-30 10:15:57 -0400

    feat(canary): log client id

--- packages/sign-client/test/canary/canary.spec.ts
+++ packages/sign-client/test/canary/canary.spec.ts
@@ -23,7 +23,9 @@ describe("Canary", () => {
       log(`Clients initialized (relay '${TEST_RELAY_URL}')`);
       const { pairingA, sessionA } = await testConnectMethod(clients);
       log(
-        `Clients connected (relay '${TEST_RELAY_URL}', pairing topic '${pairingA.topic}', session topic '${sessionA.topic}')`,
+        `Clients connected (relay '${TEST_RELAY_URL}', client ids: A:'${await clients.A.core.crypto.getClientId()}';B:'${await clients.B.core.crypto.getClientId()}' pairing topic '${
+          pairingA.topic
+        }', session topic '${sessionA.topic}')`,
       );
 
       await Promise.all([
