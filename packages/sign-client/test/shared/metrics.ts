d5f37159ef398068294aa76610242c2318893f00
Derek <alexanderderekrein@gmail.com>
2022-09-15 16:23:04 +0200

    feat(canary): granular latency metrics (#1444)

--- packages/sign-client/test/shared/metrics.ts
+++ packages/sign-client/test/shared/metrics.ts
@@ -7,61 +7,83 @@ export const uploadCanaryResultsToCloudWatch = async (
   metricsPrefix: string,
   isTestPassed: boolean,
   testDurationMs: number,
+  otherLatencies: object[],
 ) => {
   const cloudwatch = new CloudWatch({ region: "eu-central-1" });
   const ts = new Date();
+  const metrics = [
+    {
+      MetricName: `${metricsPrefix}.success`,
+      Dimensions: [
+        {
+          Name: "Target",
+          Value: target,
+        },
+        {
+          Name: "Region",
+          Value: region,
+        },
+      ],
+      Unit: "Count",
+      Value: isTestPassed ? 1 : 0,
+      Timestamp: ts,
+    },
+    {
+      MetricName: `${metricsPrefix}.failure`,
+      Dimensions: [
+        {
+          Name: "Target",
+          Value: target,
+        },
+        {
+          Name: "Region",
+          Value: region,
+        },
+      ],
+      Unit: "Count",
+      Value: isTestPassed ? 0 : 1,
+      Timestamp: ts,
+    },
+    {
+      MetricName: `${metricsPrefix}.latency`,
+      Dimensions: [
+        {
+          Name: "Target",
+          Value: target,
+        },
+        {
+          Name: "Region",
+          Value: region,
+        },
+      ],
+      Unit: "Milliseconds",
+      Value: testDurationMs,
+      Timestamp: ts,
+    },
+  ];
+
+  const latencies = otherLatencies.map((metric) => {
+    const metricName = Object.keys(metric)[0];
+    return {
+      MetricName: `${metricsPrefix}.${metricName}`,
+      Dimensions: [
+        {
+          Name: "Target",
+          Value: target,
+        },
+        {
+          Name: "Region",
+          Value: region,
+        },
+      ],
+      Unit: "Milliseconds",
+      Value: metric[metricName],
+      Timestamp: ts,
+    };
+  });
 
   const params: CloudWatch.PutMetricDataInput = {
-    MetricData: [
-      {
-        MetricName: `${metricsPrefix}.success`,
-        Dimensions: [
-          {
-            Name: "Target",
-            Value: target,
-          },
-          {
-            Name: "Region",
-            Value: region,
-          },
-        ],
-        Unit: "Count",
-        Value: isTestPassed ? 1 : 0,
-        Timestamp: ts,
-      },
-      {
-        MetricName: `${metricsPrefix}.failure`,
-        Dimensions: [
-          {
-            Name: "Target",
-            Value: target,
-          },
-          {
-            Name: "Region",
-            Value: region,
-          },
-        ],
-        Unit: "Count",
-        Value: isTestPassed ? 0 : 1,
-        Timestamp: ts,
-      },
-      {
-        MetricName: `${metricsPrefix}.latency`,
-        Dimensions: [
-          {
-            Name: "Target",
-            Value: target,
-          },
-          {
-            Name: "Region",
-            Value: region,
-          },
-        ],
-        Unit: "Milliseconds",
-        Value: testDurationMs,
-        Timestamp: ts,
-      },
-    ],
+    MetricData: [...metrics, ...latencies],
     Namespace: `${env}_Canary_SignClient`,
   };
 
