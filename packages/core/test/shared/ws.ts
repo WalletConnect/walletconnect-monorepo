dfcec7e725b3a6b79667c3b2ee74c98fde761954
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-26 19:11:02 +0300

    refactor: delete connection object

--- packages/core/test/shared/ws.ts
+++ packages/core/test/shared/ws.ts
@@ -4,6 +4,7 @@ import { throttle } from ".";
 export async function disconnectSocket(core: ICore) {
   if (core.relayer.connected) {
     await core.relayer.provider.connection.close();
+    delete core.relayer.provider.connection;
     // await abit for socket to disconnect
     await throttle(500);
   }
