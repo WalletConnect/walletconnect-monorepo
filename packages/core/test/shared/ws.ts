f8f577e2e6d73db525b7304c3f7b3db17b0f72ac
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-26 19:20:43 +0300

    refactor: rm delete & throttle

--- packages/core/test/shared/ws.ts
+++ packages/core/test/shared/ws.ts
@@ -4,8 +4,5 @@ import { throttle } from ".";
 export async function disconnectSocket(core: ICore) {
   if (core.relayer.connected) {
     await core.relayer.provider.connection.close();
-    delete core.relayer.provider.connection;
-    // await abit for socket to disconnect
-    await throttle(500);
   }
 }
