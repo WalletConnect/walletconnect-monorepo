c4c77af13a1e878c24f6355829d8fa61b327e552
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-26 13:22:21 +0300

    refactor: uses connection.close instead of disconnect

--- packages/core/test/shared/ws.ts
+++ packages/core/test/shared/ws.ts
@@ -2,8 +2,8 @@ import { IRelayer } from "@walletconnect/types";
 import { throttle } from ".";
 
 export async function disconnectSocket(relayer: IRelayer) {
-  if (relayer?.connected) {
-    await relayer?.provider.disconnect();
+  if (relayer.connected) {
+    await relayer.provider.connection.close();
   }
 
   await throttle(100);
