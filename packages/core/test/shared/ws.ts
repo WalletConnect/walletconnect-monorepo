54c9b3e46303b9e81955cd2bc9831b6d9f79d117
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-26 19:26:48 +0300

    refactor: makeshift disconnect v2

--- packages/core/test/shared/ws.ts
+++ packages/core/test/shared/ws.ts
@@ -4,5 +4,7 @@ import { throttle } from ".";
 export async function disconnectSocket(core: ICore) {
   if (core.relayer.connected) {
     await core.relayer.provider.connection.close();
+    delete core.relayer.provider.connection;
+    core.relayer.provider.connect = () => new Promise<void>((resolve) => resolve);
   }
 }
