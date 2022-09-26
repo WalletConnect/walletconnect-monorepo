8709b64ba9406a4f697e2c6df2ca986e9b5aa41b
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-26 14:15:12 +0300

    Revert "refactor: uses disconnect event"
    This reverts commit 957a970b5031d2516d564ca2d16d1e2564cdad87.

--- packages/core/test/subscriber.spec.ts
+++ packages/core/test/subscriber.spec.ts
@@ -55,7 +55,7 @@ describe("Subscriber", () => {
       expect(subscriber.subscriptions.size).to.equal(1);
       expect(subscriber.topics.length).to.equal(1);
       // relayer.provider emits a `disconnect` event -> should clear both subscriptions and topics.
-      await disconnectSocket(relayer);
+      relayer.provider.events.emit(RELAYER_PROVIDER_EVENTS.disconnect);
       expect(subscriber.subscriptions.size).to.equal(0);
       expect(subscriber.topics.length).to.equal(0);
     });
