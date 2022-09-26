58b6816e2fb2c5da96f3e37084b513052d860fbf
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-26 19:05:35 +0300

    refactor: close connection directly in ws

--- packages/core/test/shared/ws.ts
+++ packages/core/test/shared/ws.ts
@@ -3,11 +3,7 @@ import { throttle } from ".";
 
 export async function disconnectSocket(core: ICore) {
   if (core.relayer.connected) {
-    core.relayer.provider.connection?.on("open", async () => {
-      await core.relayer.provider.connection.close();
-    });
-
-    await core.relayer.provider.disconnect();
+    await core.relayer.provider.connection.close();
     // await abit for socket to disconnect
     await throttle(500);
   }
