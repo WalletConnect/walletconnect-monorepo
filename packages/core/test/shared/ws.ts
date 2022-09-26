d0ca1d87ce41cdcd99c4d139e2de776e784870be
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-26 15:29:53 +0300

    chore: adds connected status to logging

--- packages/core/test/shared/ws.ts
+++ packages/core/test/shared/ws.ts
@@ -4,10 +4,11 @@ import { RELAYER_PROVIDER_EVENTS } from "../../src";
 
 export async function disconnectSocket(relayer: IRelayer, testName = "") {
   if (relayer.connected) {
-    console.log("disconnect", testName);
     relayer.provider.events.emit(RELAYER_PROVIDER_EVENTS.disconnect);
     await relayer.provider.disconnect();
     // await abit for socket to disconnect
     await throttle(2_000);
+
+    console.log("disconnect", relayer.connected, testName);
   }
 }
