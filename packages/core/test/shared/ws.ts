c93848f8d89a493a3fe46f612024a70d9526467f
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-26 14:16:47 +0300

    feat: fire disconnect event along with ws disconnect

--- packages/core/test/shared/ws.ts
+++ packages/core/test/shared/ws.ts
@@ -1,9 +1,11 @@
 import { IRelayer } from "@walletconnect/types";
 import { throttle } from ".";
+import { RELAYER_PROVIDER_EVENTS } from "../../src";
 
 export async function disconnectSocket(relayer: IRelayer) {
   if (relayer.connected) {
     console.log("disconnect");
+    relayer.provider.events.emit(RELAYER_PROVIDER_EVENTS.disconnect);
     await relayer.provider.disconnect();
     // await abit for socket to disconnect
     await throttle(2_000);
