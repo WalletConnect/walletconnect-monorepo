e79e5e2b4575d7d7ad17e8a13467ab1625ac4b9b
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-26 13:51:14 +0300

    refactor: reuse core in subscriber tests

--- packages/core/test/subscriber.spec.ts
+++ packages/core/test/subscriber.spec.ts
@@ -10,7 +10,6 @@ import {
   CORE_DEFAULT,
   CORE_STORAGE_PREFIX,
   MESSAGES_STORAGE_VERSION,
-  Relayer,
   RELAYER_PROVIDER_EVENTS,
   Subscriber,
   SUBSCRIBER_CONTEXT,
@@ -19,9 +18,6 @@ import { disconnectSocket, TEST_CORE_OPTIONS } from "./shared";
 
 describe("Subscriber", () => {
   const logger = pino(getDefaultLoggerOptions({ level: CORE_DEFAULT.logger }));
-
-  let relayer: IRelayer;
-  let subscriber: ISubscriber;
   let core: ICore;
 
   beforeEach(async () => {
@@ -32,14 +28,12 @@ describe("Subscriber", () => {
     core = new Core(TEST_CORE_OPTIONS);
     await core.start();
 
-    relayer = core.relayer;
-    subscriber = relayer.subscriber; //new Subscriber(relayer, logger);
-    subscriber.relayer.provider.request = () => Promise.resolve({} as any);
-    await subscriber.init();
+    core.relayer.subscriber.relayer.provider.request = () => Promise.resolve({} as any);
+    await core.relayer.subscriber.init();
   });
 
   it("provides the expected `storageKey` format", () => {
-    const subscriber = new Subscriber(relayer, logger);
+    const subscriber = new Subscriber(core.relayer, logger);
     expect(subscriber.storageKey).to.equal(
       CORE_STORAGE_PREFIX + MESSAGES_STORAGE_VERSION + "//" + SUBSCRIBER_CONTEXT,
     );
@@ -49,15 +43,15 @@ describe("Subscriber", () => {
     it("registers event listeners", async () => {
       const topic = generateRandomBytes32();
       const emitSpy = Sinon.spy();
-      subscriber.events.emit = emitSpy;
+      core.relayer.subscriber.events.emit = emitSpy;
       // subscribe to a topic
-      await subscriber.subscribe(topic);
-      expect(subscriber.subscriptions.size).to.equal(1);
-      expect(subscriber.topics.length).to.equal(1);
+      await core.relayer.subscriber.subscribe(topic);
+      expect(core.relayer.subscriber.subscriptions.size).to.equal(1);
+      expect(core.relayer.subscriber.topics.length).to.equal(1);
       // relayer.provider emits a `disconnect` event -> should clear both subscriptions and topics.
-      relayer.provider.events.emit(RELAYER_PROVIDER_EVENTS.disconnect);
-      expect(subscriber.subscriptions.size).to.equal(0);
-      expect(subscriber.topics.length).to.equal(0);
+      core.relayer.provider.events.emit(RELAYER_PROVIDER_EVENTS.disconnect);
+      expect(core.relayer.subscriber.subscriptions.size).to.equal(0);
+      expect(core.relayer.subscriber.topics.length).to.equal(0);
     });
   });
 
@@ -68,15 +62,15 @@ describe("Subscriber", () => {
     beforeEach(() => {
       requestSpy = Sinon.spy(() => "test-id");
       topic = generateRandomBytes32();
-      subscriber.relayer.provider.request = requestSpy;
+      core.relayer.subscriber.relayer.provider.request = requestSpy;
     });
 
     it("throws if Subscriber was not initialized", async () => {
-      const subscriber = new Subscriber(relayer, logger);
+      const subscriber = new Subscriber(core.relayer, logger);
       await expect(subscriber.subscribe(topic)).rejects.toThrow("Not initialized. subscription");
     });
     it("calls `provider.request` with the expected request shape", async () => {
-      await subscriber.subscribe(topic);
+      await core.relayer.subscriber.subscribe(topic);
       expect(
         requestSpy.calledOnceWith(
           Sinon.match({
@@ -89,7 +83,7 @@ describe("Subscriber", () => {
       ).to.be.true;
     });
     it("returns the subscription id", async () => {
-      const id = await subscriber.subscribe(topic);
+      const id = await core.relayer.subscriber.subscribe(topic);
       expect(id).to.equal("test-id");
     });
   });
@@ -103,16 +97,16 @@ describe("Subscriber", () => {
       requestSpy = Sinon.spy();
       messageDeleteSpy = Sinon.spy();
       topic = generateRandomBytes32();
-      subscriber.relayer.provider.request = requestSpy;
-      subscriber.relayer.messages.del = messageDeleteSpy;
+      core.relayer.subscriber.relayer.provider.request = requestSpy;
+      core.relayer.subscriber.relayer.messages.del = messageDeleteSpy;
     });
     it("throws if Subscriber was not initialized", async () => {
-      const subscriber = new Subscriber(relayer, logger);
+      const subscriber = new Subscriber(core.relayer, logger);
       await expect(subscriber.unsubscribe(topic)).rejects.toThrow("Not initialized. subscription");
     });
     it("unsubscribes by individual id if `opts.id` is provided", async () => {
       const id = "test-id";
-      await subscriber.unsubscribe(topic, { id, relay: getRelayProtocolName() });
+      await core.relayer.subscriber.unsubscribe(topic, { id, relay: getRelayProtocolName() });
       expect(messageDeleteSpy.calledOnceWith(topic)).to.be.true;
       expect(
         requestSpy.calledOnceWith(
@@ -126,10 +120,10 @@ describe("Subscriber", () => {
       ).to.be.true;
     });
     it("unsubscribes by topic by default", async () => {
-      await subscriber.subscribe(topic);
-      expect(subscriber.topics.length).to.equal(1);
-      await subscriber.unsubscribe(topic);
-      expect(subscriber.topics.length).to.equal(0);
+      await core.relayer.subscriber.subscribe(topic);
+      expect(core.relayer.subscriber.topics.length).to.equal(1);
+      await core.relayer.subscriber.unsubscribe(topic);
+      expect(core.relayer.subscriber.topics.length).to.equal(0);
       expect(
         requestSpy.getCall(1).calledWith(
           Sinon.match({
