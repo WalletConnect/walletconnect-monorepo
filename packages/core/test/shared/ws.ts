04df13ae94f864ae393764775168c08cf9341268
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-26 13:40:58 +0300

    chore: log disconnects

--- packages/core/test/shared/ws.ts
+++ packages/core/test/shared/ws.ts
@@ -3,6 +3,7 @@ import { throttle } from ".";
 
 export async function disconnectSocket(relayer: IRelayer) {
   if (relayer.connected) {
+    console.log("disconnect");
     await relayer.provider.disconnect();
     // await abit for socket to disconnect
     await throttle(2_000);
