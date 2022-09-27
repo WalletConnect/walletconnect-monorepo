e92aab8cb3e6f43e7a6d40fec08cae7a997687a0
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-27 09:27:37 +0300

    chore: rm comments

--- packages/core/test/shared/ws.ts
+++ packages/core/test/shared/ws.ts
@@ -3,13 +3,8 @@ import { throttle } from ".";
 
 export async function disconnectSocket(core: ICore) {
   if (core.relayer.connected) {
-    core.relayer.provider.on("open", () => {
-      console.log("on open");
-    });
     core.relayer.provider.connect = () => new Promise<void>((resolve) => resolve);
     await core.relayer.provider.connection.close();
-
     throttle(2_000);
-    console.log("connected after", core.relayer.connected);
   }
 }
