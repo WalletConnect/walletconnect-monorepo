0007bcba1d14f5b83002667eae01a91a1067bc4f
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-28 15:46:57 +0300

    feat: adds a listener if connection is reopen to disconnect automatically

--- packages/sign-client/test/shared/ws.ts
+++ packages/sign-client/test/shared/ws.ts
@@ -3,6 +3,16 @@ import { ICore } from "@walletconnect/types";
 export async function disconnectSocket(core: ICore) {
   if (core.relayer.connected) {
     core.relayer.provider.connect = () => new Promise<void>((resolve) => resolve);
-    await core.relayer.provider.connection.close();
+    core.relayer.provider.connection.on(
+      "open",
+      async () => await disconnect(core.relayer.provider.connection),
+    );
+    await disconnect(core.relayer.provider.connection);
+  }
+}
+
+async function disconnect(socket: any) {
+  if (socket.connected) {
+    await socket.close();
   }
 }
