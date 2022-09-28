c181eb300fdaa20369f95005ef26a1ab231d0a32
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-28 18:18:33 +0300

    refator: makes socket disconnect recursive

--- packages/sign-client/test/shared/ws.ts
+++ packages/sign-client/test/shared/ws.ts
@@ -3,10 +3,7 @@ import { ICore } from "@walletconnect/types";
 export async function disconnectSocket(core: ICore) {
   if (core.relayer.connected) {
     core.relayer.provider.connect = () => new Promise<void>((resolve) => resolve);
-    core.relayer.provider.connection.on(
-      "open",
-      async () => await disconnect(core.relayer.provider.connection),
-    );
+    core.relayer.provider.connection.on("open", async () => await disconnectSocket(core));
     await disconnect(core.relayer.provider.connection);
   }
 }
