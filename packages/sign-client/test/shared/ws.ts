bb80c36728865ae9666f68a8dd4c9c966a4c9b78
Gancho Radkov <ganchoradkov93@gmail.com>
2022-09-29 13:11:21 +0300

    fix: override `.provider.connection.events` too

--- packages/sign-client/test/shared/ws.ts
+++ packages/sign-client/test/shared/ws.ts
@@ -3,8 +3,18 @@ import EventEmitter from "events";
 
 export async function disconnectSocket(core: ICore) {
   if (core.relayer.connected) {
-    // override the events to disable reconnection
+    core.relayer.provider.connect = () => new Promise<void>((resolve) => resolve);
+    core.relayer.provider.connection.on("open", async () => {
+      await disconnectSocket(core);
+    });
     core.relayer.provider.events = new EventEmitter();
-    await core.relayer.provider.connection.close();
+    core.relayer.provider.connection.events = new EventEmitter();
+    await disconnect(core.relayer.provider.connection);
+  }
+}
+
+async function disconnect(socket: any) {
+  if (socket.connected) {
+    await socket.close();
   }
 }
