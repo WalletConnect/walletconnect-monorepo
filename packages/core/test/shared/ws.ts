c37bab4ab17812fa7c6b91df1b2809ff4f7a9cd8
Gancho Radkov <ganchoradkov93@gmail.com>
2022-11-29 17:19:58 +0200

    feat: uses transportClose for socket disconnect

--- packages/core/test/shared/ws.ts
+++ packages/core/test/shared/ws.ts
@@ -1,18 +1,7 @@
-import { IJsonRpcConnection } from "@walletconnect/jsonrpc-utils";
 import { IRelayer } from "@walletconnect/types";
-import EventEmitter from "events";
 
 export async function disconnectSocket(relayer: IRelayer) {
   if (relayer.connected) {
-    relayer.provider.events = new EventEmitter();
-    relayer.core.heartbeat.events = new EventEmitter();
-    relayer.provider.connection.on("open", async () => {
-      await disconnect(relayer.provider.connection);
-    });
-    await disconnect(relayer.provider.connection);
+    await relayer.transportClose();
   }
-}
-
-function disconnect(socket: IJsonRpcConnection) {
-  return socket.close();
 }
