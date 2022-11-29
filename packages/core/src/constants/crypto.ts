1cffc85e7c094c111d45365ca210f2c4bc862557
Gancho Radkov <ganchoradkov93@gmail.com>
2022-11-29 18:02:54 +0200

    chore: restore default

--- packages/core/src/constants/crypto.ts
+++ packages/core/src/constants/crypto.ts
@@ -2,6 +2,6 @@ import { ONE_DAY } from "@walletconnect/time";
 
 export const CRYPTO_CONTEXT = "crypto";
 
-export const CRYPTO_CLIENT_SEED = `client_ed25519_seed`;
+export const CRYPTO_CLIENT_SEED = "client_ed25519_seed";
 
 export const CRYPTO_JWT_TTL = ONE_DAY;
