17dedecac901aeed5eadcf63cce57eb63317b215
Gancho Radkov <ganchoradkov93@gmail.com>
2022-12-13 09:25:49 +0200

    feat: restart on transport error

--- packages/core/src/controllers/relayer.ts
+++ packages/core/src/controllers/relayer.ts
@@ -191,6 +191,7 @@ export class Relayer extends IRelayer {
       console.log("connection restarted --- @!", this.core.name);
     } catch (e: unknown | Error) {
       console.log("transport Open catched error", e, this.core.name);
+      await this.transportOpen();
     }
   }
 
