3727da64ae1bc81827f6fb282f863c1b786dacba
Ben Kremer <ben@walletconnect.com>
2023-01-05 16:19:00 +0100

    fix: tweak string comparison operand to be Linux friendly

--- scripts/update_relayer_sdk_version.sh
+++ scripts/update_relayer_sdk_version.sh
@@ -13,7 +13,7 @@ new_value="RELAYER_SDK_VERSION = \"$next_version\""
 echo "[SCRIPT] Updating RELAYER_SDK_VERSION to $next_version in $file_location..."
 
 # Use sed to update the value in the file
-if [ "$(uname)" == "Darwin" ]; then
+if [ "$(uname)" = "Darwin" ]; then
   # MacOS requires an empty string as the second argument to -i
   sed -i "" "s/${regex}/${new_value}/g" $file_location
 else
