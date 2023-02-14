47611c0c7b806fc8c29e3d9abe70668f37a66160
Ilja <idaderko@gmail.com>
2023-02-14 10:33:55 +0200

    export method and event constants, types

--- providers/ethereum-provider/src/index.ts
+++ providers/ethereum-provider/src/index.ts
@@ -1,3 +1,5 @@
 import { EthereumProvider as Provider } from "./EthereumProvider";
 export const EthereumProvider = Provider;
+export type { RpcEvent, RpcMethod } from "./EthereumProvider";
+export { OPTIONAL_METHODS, REQUIRED_EVENTS, REQUIRED_METHODS, OPTIONAL_EVENTS } from "./constants";
 export default Provider;
