7d431240b6c1c42663aca88ebe806df0971510e5
marshacb <cameron.marshall12@gmail.com>
2023-02-22 12:52:43 -0500

    add polkadot to universal providers add case for UniversalProvider polkadot namespace

--- providers/universal-provider/src/providers/index.ts
+++ providers/universal-provider/src/providers/index.ts
@@ -2,3 +2,4 @@ export * as Eip155Provider from "./eip155";
 export * as SolanaProvider from "./solana";
 export * as CosmosProvider from "./cosmos";
 export * as CardanoProvider from "./cardano";
+export * as PolkadotProvider from './polkadot';
