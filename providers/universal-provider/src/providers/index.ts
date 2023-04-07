3da6d478d446ab18c96a461d18d57ac92182d2ab
marshacb <cameron.marshall12@gmail.com>
2023-04-07 08:09:05 -0400

    add rpc map to createHttpProvider run pretter

--- providers/universal-provider/src/providers/index.ts
+++ providers/universal-provider/src/providers/index.ts
@@ -2,4 +2,6 @@ export * as Eip155Provider from "./eip155";
 export * as SolanaProvider from "./solana";
 export * as CosmosProvider from "./cosmos";
 export * as CardanoProvider from "./cardano";
+export * as ElrondProvider from "./elrond";
+export * as MultiversXProvider from "./multiversx";
 export * as PolkadotProvider from './polkadot';
