b9de21280dfce06ba8f225146fdda383b2c834a3
Gancho Radkov <ganchoradkov@gmail.com>
2023-05-11 13:58:04 +0300

    feat: adds publisher tests

--- packages/core/test/publisher.spec.ts
+++ packages/core/test/publisher.spec.ts
@@ -5,8 +5,13 @@ import { generateRandomBytes32, hashMessage } from "@walletconnect/utils";
 import { Publisher } from "../src/controllers/publisher";
 import { HEARTBEAT_EVENTS } from "@walletconnect/heartbeat";
 
-import { Core, PUBLISHER_DEFAULT_TTL } from "../src";
+import { Core, PUBLISHER_DEFAULT_TTL, RELAYER_EVENTS } from "../src";
 import { disconnectSocket, TEST_CORE_OPTIONS, throttle } from "./shared";
+import { getBigIntRpcId } from "@walletconnect/jsonrpc-utils";
+
+const getId = () => {
+  return getBigIntRpcId().toString() as any;
+};
 
 describe("Publisher", () => {
   let core: ICore;
@@ -23,24 +28,130 @@ describe("Publisher", () => {
   });
 
   describe("init", () => {
-    it("registers event listeners", async () => {
+    it("should process queue", async () => {
       const opts = { ttl: 1, prompt: true, relay: { protocol: "irn" }, tag: 0 };
-      const itemA = { topic: generateRandomBytes32(), message: "itemA", opts };
-      const itemB = { topic: generateRandomBytes32(), message: "itemB", opts };
+      const items = [
+        {
+          topic: generateRandomBytes32(),
+          message: "itemA",
+          opts: { ...opts, id: getId() },
+        },
+        {
+          topic: generateRandomBytes32(),
+          message: "itemB",
+          opts: { ...opts, id: getId() },
+        },
+        {
+          topic: generateRandomBytes32(),
+          message: "itemC",
+          opts: { ...opts, id: getId() },
+        },
+        {
+          topic: generateRandomBytes32(),
+          message: "itemD",
+          opts: { ...opts, id: getId() },
+        },
+        {
+          topic: generateRandomBytes32(),
+          message: "itemE",
+          opts: { ...opts, id: getId() },
+        },
+      ];
+
       const requestSpy = Sinon.spy();
       publisher.relayer.request = requestSpy;
+
       // Manually set some items in the queue.
-      publisher.queue.set(hashMessage(itemA.message), itemA);
-      publisher.queue.set(hashMessage(itemB.message), itemB);
-      expect(publisher.queue.size).to.equal(2);
+      items.forEach((item) => publisher.queue.set(item.opts.id.toString(), item));
+      expect(publisher.queue.size).to.equal(items.length);
       // Emit heartbeat pulse event
       publisher.relayer.core.heartbeat.events.emit(HEARTBEAT_EVENTS.pulse);
 
-      // Using a timeout here, cannot `await` the private `rpcSubscribe` method.
-      await throttle(2_000);
-      // -> Queue should clear if pulse event is being listened for.
+      // Emit ACKs
+      items.forEach((item) =>
+        core.relayer.events.emit(RELAYER_EVENTS.message_ack, { id: item.opts.id }),
+      );
+
+      // -> Queue should clear after the ACKs.
       expect(publisher.queue.size).to.equal(0);
-      expect(requestSpy.callCount).to.equal(2);
+
+      // emit multiple pulses to ensure queue is cleared
+      await throttle(100);
+      // Emit heartbeat pulse event
+      publisher.relayer.core.heartbeat.events.emit(HEARTBEAT_EVENTS.pulse);
+      await throttle(100);
+      // Emit heartbeat pulse event
+      publisher.relayer.core.heartbeat.events.emit(HEARTBEAT_EVENTS.pulse);
+
+      // -> Queue should still be clear after the pulses.
+      expect(publisher.queue.size).to.equal(0);
+      // -> `request` should not have been called more times than the n items regardless of the n of pulses.
+      expect(requestSpy.callCount).to.equal(items.length);
+    });
+    it("should process queue with delayed ACK", () => {
+      const opts = { ttl: 1, prompt: true, relay: { protocol: "irn" }, tag: 0 };
+      const items = [
+        {
+          topic: generateRandomBytes32(),
+          message: "itemA",
+          opts: { ...opts, id: getId() },
+        },
+        {
+          topic: generateRandomBytes32(),
+          message: "itemB",
+          opts: { ...opts, id: getId() },
+        },
+        {
+          topic: generateRandomBytes32(),
+          message: "itemC",
+          opts: { ...opts, id: getId() },
+        },
+        {
+          topic: generateRandomBytes32(),
+          message: "itemD",
+          opts: { ...opts, id: getId() },
+        },
+        {
+          topic: generateRandomBytes32(),
+          message: "itemE",
+          opts: { ...opts, id: getId() },
+        },
+      ];
+
+      const requestSpy = Sinon.spy();
+      publisher.relayer.request = requestSpy;
+
+      // Manually set some items in the queue.
+      items.forEach((item) => publisher.queue.set(item.opts.id.toString(), item));
+      expect(publisher.queue.size).to.equal(items.length);
+
+      const pulsesBeforeAck = 5;
+      // emit multiple pulses to ensure queue is works correctly if ACK is delayed
+      Array.from(Array(pulsesBeforeAck).keys()).forEach(async () => {
+        publisher.relayer.core.heartbeat.events.emit(HEARTBEAT_EVENTS.pulse);
+        await throttle(100);
+      });
+
+      // Emit ACKs
+      items.forEach((item) =>
+        core.relayer.events.emit(RELAYER_EVENTS.message_ack, { id: item.opts.id }),
+      );
+
+      // -> Queue should clear after the ACKs.
+      expect(publisher.queue.size).to.equal(0);
+      // -> all requests should have been sent once per pulse
+      const expectedCallCount = items.length * pulsesBeforeAck;
+      expect(requestSpy.callCount).to.equal(expectedCallCount);
+
+      const pulsesAfterAck = 5;
+      // emit additional pulses
+      Array.from(Array(pulsesAfterAck).keys()).forEach(async () => {
+        publisher.relayer.core.heartbeat.events.emit(HEARTBEAT_EVENTS.pulse);
+        await throttle(100);
+      });
+
+      // request count should stay the same even after additional pulses
+      expect(requestSpy.callCount).to.equal(expectedCallCount);
     });
   });
 
@@ -56,7 +167,8 @@ describe("Publisher", () => {
 
     it("calls `provider.request` with the expected request shape", async () => {
       const message = "test message";
-      await publisher.publish(topic, message);
+      const id = getId();
+      await publisher.publish(topic, message, { id });
       expect(requestSpy.callCount).to.equal(1);
       expect(requestSpy.getCall(0).args[0]).to.deep.equal({
         method: "irn_publish",
@@ -67,11 +179,12 @@ describe("Publisher", () => {
           ttl: PUBLISHER_DEFAULT_TTL,
           tag: 0,
         },
+        id,
       });
     });
     it("allows overriding of defaults via `opts` param", async () => {
       const message = "test message";
-      const opts = { ttl: 1, prompt: true, relay: { protocol: "irn" }, tag: 1 };
+      const opts = { ttl: 1, prompt: true, relay: { protocol: "irn" }, tag: 1, id: getId(1) };
       await publisher.publish(topic, message, opts);
       expect(requestSpy.callCount).to.equal(1);
       expect(requestSpy.getCall(0).args[0]).to.deep.equal({
@@ -83,6 +196,7 @@ describe("Publisher", () => {
           ttl: opts.ttl,
           tag: opts.tag,
         },
+        id: opts.id,
       });
     });
   });
