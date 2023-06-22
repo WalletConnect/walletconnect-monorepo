11688293740fcb3e110d5c1037e53bfb1b6ae32b
Ben Kremer <ben@walletconnect.com>
2023-06-22 10:01:24 +0200

    Update packages/core/src/controllers/relayer.ts
    Co-authored-by: Gancho Radkov <43912948+ganchoradkov@users.noreply.github.com>

--- packages/core/src/controllers/relayer.ts
+++ packages/core/src/controllers/relayer.ts
@@ -253,7 +253,7 @@ export class Relayer extends IRelayer {
   }
 
   private rejectTransportOpen() {
-    throw new Error("Attempt to connect to relay via `transportOpen` has stalled.");
+    throw new Error("Attempt to connect to relay via `transportOpen` has stalled. Retrying...");
   }
 
   private async createProvider() {
