2ca2e7838b529f0fd8a208093e5df23f3c16e3b4
Gancho Radkov <ganchoradkov@gmail.com>
2023-07-13 12:24:44 +0300

    Revert "chore: skips test"
    This reverts commit aeaece47d013525b443de8a95d81e4c44452ad89.

--- packages/sign-client/test/sdk/client.spec.ts
+++ packages/sign-client/test/sdk/client.spec.ts
@@ -207,7 +207,7 @@ describe("Sign Client Integration", () => {
           await clients.B.ping({ topic });
           await deleteClients(clients);
         });
-        it.skip("can get pending session request", async () => {
+        it("can get pending session request", async () => {
           const clients = await initTwoClients({}, {}, { logger: "error" });
           const {
             sessionA: { topic },
