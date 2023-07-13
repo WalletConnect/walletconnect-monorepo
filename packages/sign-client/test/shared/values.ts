64225d1bba90b5bc82a92b83780733be532e175b
Gancho Radkov <ganchoradkov@gmail.com>
2023-07-13 10:25:13 +0300

    chore: debug log

--- packages/sign-client/test/shared/values.ts
+++ packages/sign-client/test/shared/values.ts
@@ -18,7 +18,7 @@ export const TEST_PROJECT_ID = process.env.TEST_PROJECT_ID
   : undefined;
 
 export const TEST_SIGN_CLIENT_OPTIONS: SignClientTypes.Options = {
-  logger: "error",
+  logger: "debug",
   relayUrl: TEST_RELAY_URL,
   projectId: TEST_PROJECT_ID,
   storageOptions: {
