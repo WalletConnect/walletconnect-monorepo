5b78ef96e70794a3be98b8400c734b1b3ed92cfc
Gancho Radkov <ganchoradkov@gmail.com>
2023-11-29 16:15:47 +0200

    feat: export session store from sign

--- packages/sign-client/src/index.ts
+++ packages/sign-client/src/index.ts
@@ -1,5 +1,6 @@
 import { SignClient as Client } from "./client";
-
+import { Session } from "./controllers/session";
+export const SessionStore = Session;
 export * from "./constants";
 
 export const SignClient = Client;
