71ae21807d140cfe06cd425f6e2568a67dcf03ec
Gancho Radkov <ganchoradkov@gmail.com>
2023-12-21 13:23:42 +0200

    refactor: uses try/catch to handle async and explicitly checks if id is provided before trying to cleanup pending request

--- packages/sign-client/src/controllers/engine.ts
+++ packages/sign-client/src/controllers/engine.ts
@@ -1044,7 +1044,7 @@ export class Engine extends IEngine {
   };
 
   private cleanupAfterResponse = (params: EngineTypes.RespondParams) => {
-    this.deletePendingSessionRequest(params.response?.id, { message: "fulfilled", code: 0 });
+    this.deletePendingSessionRequest(params.response.id, { message: "fulfilled", code: 0 });
     // intentionally delay the emitting of the next pending request a bit
     setTimeout(() => {
       this.sessionRequestQueue.state = ENGINE_QUEUE_STATES.idle;
@@ -1393,11 +1393,13 @@ export class Engine extends IEngine {
       throw new Error(message);
     }
     const { topic, response } = params;
-    // if the session is already disconnected, we can't respond to the request so we need to delete it
-    await this.isValidSessionTopic(topic).catch((error) => {
-      this.cleanupAfterResponse(params);
+    try {
+      // if the session is already disconnected, we can't respond to the request so we need to delete it
+      await this.isValidSessionTopic(topic);
+    } catch (error) {
+      if (params?.response?.id) this.cleanupAfterResponse(params);
       throw error;
-    });
+    }
     if (!isValidResponse(response)) {
       const { message } = getInternalError(
         "MISSING_OR_INVALID",
