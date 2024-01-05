7b8942c6b2b48ca558087390f896c507f53ae57d
Gancho Radkov <ganchoradkov@gmail.com>
2024-01-05 11:36:47 +0200

    Revert "fix: awaits sending session proposal before returning connection URI to avoid cases where publish might be delayed or fail"
    This reverts commit 89cf1afd2e86fcccf0ff598fe5fa0c3793be0710.

--- packages/sign-client/src/controllers/engine.ts
+++ packages/sign-client/src/controllers/engine.ts
@@ -206,7 +206,6 @@ export class Engine extends IEngine {
       topic,
       method: "wc_sessionPropose",
       params: proposal,
-      throwOnFailedPublish: true,
     });
 
     const expiry = calcExpiry(FIVE_MINUTES);
