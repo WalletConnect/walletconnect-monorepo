d5b75120bb171870344a01ddf36af535e3d50163
Gancho Radkov <ganchoradkov@gmail.com>
2024-01-25 15:28:54 +0200

    refactor: cleanup logs

--- packages/core/src/controllers/relayer.ts
+++ packages/core/src/controllers/relayer.ts
@@ -77,13 +77,7 @@ export class Relayer extends IRelayer {
   private connectionStatusPollingInterval = 20;
   private staleConnectionErrors = ["socket hang up", "socket stalled"];
   private hasExperiencedNetworkDisruption = false;
-  private requestsInFlight = new Map<
-    number,
-    {
-      promise: Promise<any>;
-      request: RequestArguments<RelayJsonRpc.SubscribeParams>;
-    }
-  >();
+  private requestsInFlight = new Map<number, Promise<unknown>>();
 
   constructor(opts: RelayerOptions) {
     super(opts);
@@ -182,10 +176,7 @@ export class Relayer extends IRelayer {
     this.logger.debug(`Publishing Request Payload`);
     const id = request.id as number;
     const requestPromise = this.provider.request(request);
-    this.requestsInFlight.set(id, {
-      promise: requestPromise,
-      request,
-    });
+    this.requestsInFlight.set(id, requestPromise);
     try {
       await this.toEstablishConnection();
       const result = await requestPromise;
@@ -223,15 +214,7 @@ export class Relayer extends IRelayer {
   public async transportClose() {
     // wait for all requests to finish before closing the transport
     if (this.requestsInFlight.size > 0) {
-      const identifier = Math.random().toString(36).substring(7);
-      console.log(
-        `${identifier} | ${this.core.name} - waiting for requests to finish: ${this.requestsInFlight.size}`,
-      );
-      this.requestsInFlight.forEach(async (value) => {
-        console.log(`${identifier} | ${this.core.name} - waiting for request`, value.request);
-        await value.promise;
-      });
-      console.log(`${identifier} | ${this.core.name} - requests finished`);
+      await Promise.all(this.requestsInFlight.values());
     }
 
     this.transportExplicitlyClosed = true;
