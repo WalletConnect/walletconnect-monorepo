55f64c97b83c3329cebf94f91fa74e70e859c279
Gancho Radkov <ganchoradkov@gmail.com>
2024-01-25 16:15:38 +0200

    chore: enable queue test

--- packages/sign-client/test/sdk/client.spec.ts
+++ packages/sign-client/test/sdk/client.spec.ts
@@ -165,7 +165,6 @@ describe("Sign Client Integration", () => {
       // 7. attempt to pair again with the same URI
       // 8. should receive an error the pairing already exists
       await expect(wallet.pair({ uri })).rejects.toThrowError();
-
       await deleteClients({ A: dapp, B: wallet });
     });
   });
@@ -352,7 +351,7 @@ describe("Sign Client Integration", () => {
          * while session request is being approved
          * the queue should continue operating normally after the `respond` rejection
          */
-        it.skip("continue processing requests queue after respond rejection due to disconnected session", async () => {
+        it("continue processing requests queue after respond rejection due to disconnected session", async () => {
           // create the clients and pair them
           const {
             clients,
