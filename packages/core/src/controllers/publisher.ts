57d6606b638ab9d3f01064523164058728a7a5bf
Gancho Radkov <ganchoradkov@gmail.com>
2024-01-25 15:28:43 +0200

    refactor: increase publish timeout

--- packages/core/src/controllers/publisher.ts
+++ packages/core/src/controllers/publisher.ts
@@ -20,7 +20,7 @@ export class Publisher extends IPublisher {
   public name = PUBLISHER_CONTEXT;
   public queue = new Map<string, PublisherTypes.Params>();
 
-  private publishTimeout = toMiliseconds(TEN_SECONDS + 5);
+  private publishTimeout = toMiliseconds(TEN_SECONDS + TEN_SECONDS);
   private needsTransportRestart = false;
 
   constructor(public relayer: IRelayer, public logger: Logger) {
