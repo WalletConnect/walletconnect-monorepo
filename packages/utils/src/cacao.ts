61a9117cc261331c7c7c9cc7472787ea70a51f30
Gancho Radkov <ganchoradkov@gmail.com>
2024-02-23 10:50:30 +0200

    fix: doesn't append new line after `Resources:` if array is empty

--- packages/utils/src/cacao.ts
+++ packages/utils/src/cacao.ts
@@ -40,7 +40,7 @@ export const formatMessage = (cacao: AuthTypes.PayloadParams, iss: string) => {
   const nonce = `Nonce: ${cacao.nonce}`;
   const issuedAt = `Issued At: ${cacao.iat}`;
   const resources = cacao.resources
-    ? `Resources:\n${cacao.resources.map((resource) => `- ${resource}`).join("\n")}`
+    ? `Resources:${cacao.resources.map((resource) => `\n- ${resource}`).join("\n")}`
     : undefined;
 
   if (cacao.resources && cacao.resources.length) {
