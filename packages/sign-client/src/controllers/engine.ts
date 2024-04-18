5d54daa7c9324d5cd86b8aa92d28e99f2a749d19
Gancho Radkov <ganchoradkov@gmail.com>
2024-04-18 10:26:03 +0300

    chore: await update

--- packages/sign-client/src/controllers/engine.ts
+++ packages/sign-client/src/controllers/engine.ts
@@ -396,7 +396,7 @@ export class Engine extends IEngine {
     // This allows the client to use the updated session like emitting events
     // without waiting for the peer to acknowledge
     await this.client.session.update(topic, { namespaces });
-    this.sendRequest({
+    await this.sendRequest({
       topic,
       method: "wc_sessionUpdate",
       params: { namespaces },
