0cf5d1bb96e48e7472b796312a45aa4e25ef57f5
Diana Savatina <diana.savvatina@trili.tech>
2024-09-10 17:07:28 +0100

    feat: added Tezos to universal provider

--- providers/universal-provider/src/UniversalProvider.ts
+++ providers/universal-provider/src/UniversalProvider.ts
@@ -20,6 +20,8 @@ import CardanoProvider from "./providers/cardano";
 import ElrondProvider from "./providers/elrond";
 import MultiversXProvider from "./providers/multiversx";
 import NearProvider from "./providers/near";
+import TezosProvider from "./providers/tezos";
+import GenericProvider from "./providers/generic";
 
 import {
   IUniversalProvider,
@@ -34,7 +36,7 @@ import {
   AuthenticateParams,
 } from "./types";
 
-import { RELAY_URL, LOGGER, STORAGE, PROVIDER_EVENTS } from "./constants";
+import { RELAY_URL, LOGGER, STORAGE, PROVIDER_EVENTS, GENERIC_SUBPROVIDER_NAME } from "./constants";
 import EventEmitter from "events";
 import { formatJsonRpcResult } from "@walletconnect/jsonrpc-utils";
 
@@ -231,7 +233,13 @@ export class UniversalProvider implements IUniversalProvider {
       // ignore without active session
       if (!this.session) return;
       const [namespace, chainId] = this.validateChain(chain);
-      this.getProvider(namespace).setDefaultChain(chainId, rpcUrl);
+      const provider = this.getProvider(namespace);
+      // @ts-expect-error
+      if (provider.name === GENERIC_SUBPROVIDER_NAME) {
+        provider.setDefaultChain(`${namespace}:${chainId}`, rpcUrl);
+      } else {
+        provider.setDefaultChain(chainId, rpcUrl);
+      }
     } catch (error) {
       // ignore the error if the fx is used prematurely before namespaces are set
       if (!/Please call connect/.test((error as Error).message)) throw error;
@@ -367,6 +375,20 @@ export class UniversalProvider implements IUniversalProvider {
             namespace: combinedNamespace,
           });
           break;
+        case "tezos":
+          this.rpcProviders[namespace] = new TezosProvider({
+            namespace: combinedNamespace,
+          });
+          break;
+        }
+        default:
+          if (!this.rpcProviders[GENERIC_SUBPROVIDER_NAME]) {
+            this.rpcProviders[GENERIC_SUBPROVIDER_NAME] = new GenericProvider({
+              namespace: combinedNamespace,
+            });
+          } else {
+            this.rpcProviders[GENERIC_SUBPROVIDER_NAME].updateNamespace(combinedNamespace);
+          }
       }
     });
   }
@@ -429,10 +451,7 @@ export class UniversalProvider implements IUniversalProvider {
   }
 
   private getProvider(namespace: string): IProvider {
-    if (!this.rpcProviders[namespace]) {
-      throw new Error(`Provider not found: ${namespace}`);
-    }
-    return this.rpcProviders[namespace];
+    return this.rpcProviders[namespace] || this.rpcProviders[GENERIC_SUBPROVIDER_NAME];
   }
 
   private onSessionUpdate(): void {
