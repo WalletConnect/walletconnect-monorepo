21ae97c4dc45f82195c58d19400ff0737649e09b
Cali93 <32299095+Cali93@users.noreply.github.com>
2024-09-26 17:06:14 +0800

    chore(dot-org): use walletconnect.org for the relay and rpc urls

--- packages/sign-client/test/shared/values.ts
+++ packages/sign-client/test/shared/values.ts
@@ -1,4 +1,4 @@
-import { SignClientTypes, RelayerTypes } from "@walletconnect/types";
+import { RelayerTypes, SignClientTypes } from "@walletconnect/types";
 
 export const PACKAGE_NAME = "sign-client";
 
@@ -6,9 +6,9 @@ export const TEST_RELAY_URL = process.env.TEST_RELAY_URL
   ? process.env.TEST_RELAY_URL
   : "ws://0.0.0.0:5555";
 
-export const TEST_RELAY_URL_US = "wss://us-east-1.relay.walletconnect.com";
-export const TEST_RELAY_URL_EU = "wss://eu-central-1.relay.walletconnect.com";
-export const TEST_RELAY_URL_AP = "wss://ap-southeast-1.relay.walletconnect.com";
+export const TEST_RELAY_URL_US = "wss://us-east-1.relay.walletconnect.org";
+export const TEST_RELAY_URL_EU = "wss://eu-central-1.relay.walletconnect.org";
+export const TEST_RELAY_URL_AP = "wss://ap-southeast-1.relay.walletconnect.org";
 
 // See https://github.com/WalletConnect/push-webhook-test-server
 export const TEST_WEBHOOK_ENDPOINT = "https://webhook-push-test.walletconnect.com/";
