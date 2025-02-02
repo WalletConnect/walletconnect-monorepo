fbe9be9a07cf36960e449430add2804668277647
a <a@tuxpa.in>
2025-02-02 02:40:23 -0600

    switch to es-toolkit

--- providers/universal-provider/src/utils/misc.ts
+++ providers/universal-provider/src/utils/misc.ts
@@ -8,7 +8,7 @@ import {
 } from "@walletconnect/utils";
 import { RPC_URL } from "../constants";
 import { Namespace, NamespaceConfig } from "../types";
-import merge from "lodash/merge";
+import { merge } from "es-toolkit";
 
 export function getRpcUrl(chainId: string, rpc: Namespace, projectId?: string): string | undefined {
   const chain = parseChainId(chainId);
