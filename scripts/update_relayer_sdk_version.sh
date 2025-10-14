fb1d0803d0dab09ee076571081dbfafa7fe3d97c
Dmitry <98899785+mdqst@users.noreply.github.com>
2025-10-14 19:24:28 +0300

    refactor: make the script simpler and more reliable

--- scripts/update_relayer_sdk_version.sh
+++ scripts/update_relayer_sdk_version.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Context: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -Eeuo pipefail
 
 file_location="packages/core/src/constants/relayer.ts"
-regex="RELAYER_SDK_VERSION = \".*\""
+regex='RELAYER_SDK_VERSION = ".*"'
 
 # Get the next version from lerna.json.
 lerna_file="lerna.json"
-next_version=$(grep -E '"version": "(.*)"' $lerna_file | sed -E 's/"version": "(.*)"/\1/' | sed 's/^[[:space:]]*//')
+next_version=$(grep -Eo '"version": *"[^"]+"' "$lerna_file" | sed -E 's/.*"version": *"([^"]+)".*/\1/')
 
 # Define the replace value
 new_value="RELAYER_SDK_VERSION = \"$next_version\""
@@ -15,11 +15,10 @@ new_value="RELAYER_SDK_VERSION = \"$next_version\""
 echo "[SCRIPT] Updating RELAYER_SDK_VERSION to $next_version in $file_location..."
 
 # Use sed to update the value in the file
-if [ "$(uname)" = "Darwin" ]; then
-  # MacOS requires an empty string as the second argument to -i
-  sed -i "" "s/${regex}/${new_value}/g" $file_location
+if sed --version >/dev/null 2>&1; then
+  sed -i "s|$regex|$new_value|g" "$file_location"
 else
-  sed -i "s/${regex}/${new_value}/g" $file_location
+  sed -i '' "s|$regex|$new_value|g" "$file_location"
 fi
 
 echo "[SCRIPT] ...Done!"
